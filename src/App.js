b11083c3d35f17b478ef9a738fda656aa6019b40
pradishan <k.pradeeshan4@gmail.com>
2023-07-15 11:21:22 +0530

    pages routed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-// import logo from './logo.svg';
 import "./App.css";
-import "./components/admin/Admin";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./components/admin/Admin";
+import Doctorinterface from "./components/doctorinterface/Doctorinterface";
+import Inventory from "./components/inventoryinterface/Inventory";
+import Clubs from "./components/clubs/Clubs";
+import Profile from "./components/profile/Profile";
+import Login from "./components/login/Login";
+import Home from "./components/home/Home";
+
 function App() {
   return (
-    <div className="App">
-      <Admin />
-    </div>
+    <Router>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctor" element={<Doctorinterface />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/clubs" element={<Clubs />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
