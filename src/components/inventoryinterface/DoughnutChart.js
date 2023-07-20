171e5d48a5c7ba9ad22d4e73079cc800fa37bf23
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-07-20 13:49:21 +0530

    doughnut chart error fixed

--- src/components/inventoryinterface/DoughnutChart.js
+++ src/components/inventoryinterface/DoughnutChart.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
+// DoughnutChart.js
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import {Chart, ArcElement} from 'chart.js';
+Chart.register(ArcElement);
 
 const DoughnutChart = () => {
     const data = {
-        labels: ['Red', 'Blue', 'Yellow'],
+        labels: ["Red", "Yellow", "Blue"],
         datasets: [
             {
-                data: [300, 200, 100],
-                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-                hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                data: [35, 20, 5],
+                backgroundColor: ["#CE0000", "#FCC400", "#0300A0"],
+                hoverBackgroundColor: ["#ff0000", "#fffa00", "#0022ff"],
             },
         ],
     };
 
-    return <Doughnut data={data} />;
+    const options = {
+        maintainAspectRatio: false,
+        responsive: true,
+    };
+
+    return <Doughnut data={data} options={options} />;
 };
 
 export default DoughnutChart;
+
