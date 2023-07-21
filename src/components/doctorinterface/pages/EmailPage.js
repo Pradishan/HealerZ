a0acd76b43eb18534ef627a4dcd0117a77ac9d50
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2023-07-21 20:06:14 +0530

    email text remove

--- src/components/doctorinterface/pages/EmailPage.js
+++ src/components/doctorinterface/pages/EmailPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FeatherIcon from 'feather-icons-react';
+import Demail from '../Demail';
 
 const scrollMedTable = {
   maxHeight: '10rem',
@@ -9,8 +10,7 @@ const scrollMedTable = {
 export default function EmailPage() {
   return (
     <>
-      <div className='bg-white p-3 rounded m-0 '>
-        Email Page
+      <div className='bg-white p-3 rounded m-2 '>
         <div className='d-flex align-items-center justify-content-between'>
           <h5 className='mt-2'>Prescription</h5>
           <div className='d-flex align-items-center'>
@@ -22,69 +22,66 @@ export default function EmailPage() {
         </div>
 
         {/* tabele */}
-        <table class="table table-hover px-2">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">NAME</th>
-              <th scope="col">DATE</th>
-              <th scope="col">DESCRIPTION</th>
-              <th scope="col">STATUS</th>
-              <th scope="col">ACTION</th>
-            </tr>
-          </thead>
-          <tbody style={scrollMedTable}>
-            <tr className=''>
-              <td>cst20001</td>
-              <td>Mark</td>
-              <td>26-12-2013</td>
-              <td>Technophobia Virus" or "Technophobia Syndrome"</td>
-              <td>requested</td>
-              <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
-            </tr>
-            <tr className=''>
-              <td>cst20001</td>
-              <td>Mark</td>
-              <td>26-12-2013</td>
-              <td>Technophobia Virus" or "Technophobia Syndrome"</td>
-              <td>requested</td>
-              <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
-            </tr>
-            <tr className=''>
-              <td>cst20001</td>
-              <td>Mark</td>
-              <td>26-12-2013</td>
-              <td>Technophobia Virus" or "Technophobia Syndrome"</td>
-              <td>requested</td>
-              <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
-            </tr>
-            <tr className=''>
-              <td>cst20001</td>
-              <td>Mark</td>
-              <td>26-12-2013</td>
-              <td>Technophobia Virus" or "Technophobia Syndrome"</td>
-              <td>requested</td>
-              <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
-            </tr>
-            <tr className=''>
-              <td>cst20001</td>
-              <td>Mark</td>
-              <td>26-12-2013</td>
-              <td>Technophobia Virus" or "Technophobia Syndrome"</td>
-              <td>requested</td>
-              <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
-            </tr>
-          </tbody>
-        </table>
-        <div className='d-flex align-items-center'>
-          <h5 className='m-2'>Email</h5>
-          <div className='d-flex align-items-center'>
-            <div className='input-group-text bg-gray border-0 rounded-pill'>
-              <input type='text' className='form-control rounded-pill border-0 bg-gray' placeholder='Receptionists' />
-              <FeatherIcon icon="at-sign" className='me-2 text-muted'/>
-            </div>
-          </div>
+        <div className='px-2'>
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">NAME</th>
+                <th scope="col">DATE</th>
+                <th scope="col">DESCRIPTION</th>
+                <th scope="col">STATUS</th>
+                <th scope="col">ACTION</th>
+              </tr>
+            </thead>
+            <tbody style={scrollMedTable}>
+              <tr className=''>
+                <td>cst20001</td>
+                <td>Mark</td>
+                <td>26-12-2013</td>
+                <td>Technophobia Virus" or "Technophobia Syndrome"</td>
+                <td>requested</td>
+                <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
+              </tr>
+              <tr className=''>
+                <td>cst20001</td>
+                <td>Mark</td>
+                <td>26-12-2013</td>
+                <td>Technophobia Virus" or "Technophobia Syndrome"</td>
+                <td>requested</td>
+                <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
+              </tr>
+              <tr className=''>
+                <td>cst20001</td>
+                <td>Mark</td>
+                <td>26-12-2013</td>
+                <td>Technophobia Virus" or "Technophobia Syndrome"</td>
+                <td>requested</td>
+                <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
+              </tr>
+              <tr className=''>
+                <td>cst20001</td>
+                <td>Mark</td>
+                <td>26-12-2013</td>
+                <td>Technophobia Virus" or "Technophobia Syndrome"</td>
+                <td>requested</td>
+                <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
+              </tr>
+              <tr className=''>
+                <td>cst20001</td>
+                <td>Mark</td>
+                <td>26-12-2013</td>
+                <td>Technophobia Virus" or "Technophobia Syndrome"</td>
+                <td>requested</td>
+                <td><button className='btn text-white shadow btn-gr p-1'>View</button></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+
+        {/* Email */}
+        <Demail />
+
         <div className='d-flex justify-content-between align-items-center'>
           <p className='m-0'>DR.V.K.Pradishan MBBS</p>
           <button className='btn w-25 text-white shadow btn-gr'>Send</button>
