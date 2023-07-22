04a8d41dca50be95fbcdfb3529a86a20936f00bf
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-07-23 00:12:05 +0530

    frontend finished1

--- src/components/inventoryinterface/UpdateModal.js
+++ src/components/inventoryinterface/UpdateModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+  import React from "react";
 import { Modal, Button } from "react-bootstrap";
 import './inventory.css';
 import SelectInput from "./additional/SelectInput";
