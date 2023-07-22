a6564d99c0d0bc9e2ea1e93f9f791442399e2109
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2023-07-22 21:17:14 +0530

    hover effect in dtabs

--- src/components/doctorinterface/Dtabs.js
+++ src/components/doctorinterface/Dtabs.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Dtabs.js';
 import TestPage from './pages/TestPage.js';
 import EmailPage from './pages/EmailPage.js';
 import MedicalPage from './pages/MedicalPage.js';
@@ -8,14 +7,14 @@ export default function Dtabs() {
     return (
         <>
             <ul className="nav nav-tabs" id="myTab" role="tablist">
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link active text-dark" id="test-tab" data-bs-toggle="tab" data-bs-target="#test-tab-pane" type="button" role="tab" aria-controls="test-tab-pane" aria-selected="true"><p className='nav-hover m-0'>Test</p></button>
+                <li className="nav-item nav-hover" role="presentation">
+                    <button className="nav-link active text-dark" id="test-tab" data-bs-toggle="tab" data-bs-target="#test-tab-pane" type="button" role="tab" aria-controls="test-tab-pane" aria-selected="true"><p className='m-0'>Test</p></button>
                 </li>
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link text-dark" id="email-tab" data-bs-toggle="tab" data-bs-target="#email-tab-pane" type="button" role="tab" aria-controls="email-tab-pane" aria-selected="false"><p className='nav-hover m-0'>Email</p></button>
+                <li className="nav-item nav-hover" role="presentation">
+                    <button className="nav-link text-dark" id="email-tab" data-bs-toggle="tab" data-bs-target="#email-tab-pane" type="button" role="tab" aria-controls="email-tab-pane" aria-selected="false"><p className='m-0'>Email</p></button>
                 </li>
-                <li className="nav-item" role="presentation">
-                    <button className="nav-link text-dark" id="medical-tab" data-bs-toggle="tab" data-bs-target="#medical-tab-pane" type="button" role="tab" aria-controls="medical-tab-pane" aria-selected="false"><p className='nav-hover m-0'>Medical</p></button>
+                <li className="nav-item nav-hover" role="presentation">
+                    <button className="nav-link text-dark" id="medical-tab" data-bs-toggle="tab" data-bs-target="#medical-tab-pane" type="button" role="tab" aria-controls="medical-tab-pane" aria-selected="false"><p className='m-0'>Medical</p></button>
                 </li>
             </ul>
             <div className="tab-content" id="myTabContent">
