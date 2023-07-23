e3afc0dc2149f8717bb9ad3e9b073ebcad7ca7c3
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2023-07-23 15:12:02 +0530

    icon hover effect

--- src/components/doctorinterface/Dsearch.js
+++ src/components/doctorinterface/Dsearch.js
@@ -10,7 +10,7 @@ export default function Dsearch() {
             <div className='bg-white rounded shadow p-3' style={width}>
                 <div className='input-group-text bg-gray border-0 rounded-pill' width='200px'>
                     <input type='text' className='form-control rounded-pill border-0 bg-gray' placeholder='Search ID' id='serarchId' />
-                    <FeatherIcon icon="search" className='mx-2 text-muted' type='button' />
+                    <FeatherIcon icon="search" className='mx-2 text-muted icon-btn' type='button' />
                 </div>
             </div>
         </>
--- src/components/doctorinterface/pages/MedicalPage.js
+++ src/components/doctorinterface/pages/MedicalPage.js
@@ -24,7 +24,7 @@ export default function MedicalPage() {
           <div className='d-flex align-items-center'>
             <div className='input-group-text bg-gray border-0 rounded-pill'>
               <input type='text' className='form-control rounded-pill border-0 bg-gray' placeholder='Search Request' id='requestSearch' />
-              <FeatherIcon icon="search" className='mx-2 text-muted' type='button' />
+              <FeatherIcon icon="search" className='mx-2 text-muted icon-btn' type='button' />
             </div>
           </div>
         </div>
