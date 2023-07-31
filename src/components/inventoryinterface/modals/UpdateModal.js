79c71c1b09658966c912423bf77b500caa2893f2
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-07-31 23:36:13 +0530

    some chnages

--- src/components/inventoryinterface/modals/UpdateModal.js
+++ src/components/inventoryinterface/modals/UpdateModal.js
@@ -1,47 +1,55 @@
-  import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 import '../inventory.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
 
 function UpdateModal(props) {
-    // const notify = () => toast("Item Updated Successfully!");
     const notify1 = () => toast("Item Deleted Successfully!");
+    const notify2 = () => toast("Item Updated Successfully!");
 
-
- const navigate = useNavigate();
-    const [inputs, setInputs] = useState([]);
-
-    const {Drug_ID} = useParams();
+    const [inputs, setInputs] = useState({});
 
     useEffect(() => {
-        getUser();
-    }, []);
-
-    function getUser() {
-        axios.get(`http://localhost/HealerZ/PHP/edit.php/${Drug_ID}`).then(function(response) {
-            console.log(response.data);
-            setInputs(response.data);
-        });
-    }
+        setInputs(props.inputs);
+    }, [props.inputs]);
 
     const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}));
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        axios.put(`http://localhost/HealerZ/PHP/edit.php/${Drug_ID}/edit`, inputs).then(function(response){
-            console.log(response.data);
-            navigate('/');
+        setInputs({
+            ...inputs,
+            [event.target.name]: event.target.value
         });
-        
-    }
-    
+    };
+
+    const handleUpdate = () => {
+        // Perform the update action here using the inputs state
+        // Example: You can use axios to send a POST request to your backend PHP script with the updated data.
+        axios.post(`http://localhost/HealerZ/PHP/update.php`, inputs)
+            .then(function (response) {
+                console.log(response.data);
+                // Handle the success notification here
+                notify2();
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    };
+
+    const handleDelete = () => {
+        // Perform the delete action here using the inputs.Drug_ID
+        // Example: You can use axios to send a DELETE request to your backend PHP script.
+        axios.delete(`http://localhost/HealerZ/PHP/delete.php?Drug_ID=${inputs.Drug_ID}`)
+            .then(function (response) {
+                console.log(response.data);
+                // Handle the success notification here
+                notify1();
+                // Handle any other logic after successful deletion
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    };
 
     const { show, onHide } = props;
     return (
@@ -50,44 +58,40 @@ function UpdateModal(props) {
                 <Modal.Title>Drug UPDATE</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <hr/>
+                <hr />
                 <form>
                     <table className={"ADDTable"}>
                         <tr>
                             <th>Drug_ID</th>
-                            <th className={"inputfield"}> <p value={Drug_ID}></p><br/></th>
+                            <th className={"inputfield"}><input type={"text"} name={"Drug_ID"} value={inputs.Drug_ID} placeholder={"DRUGXXXXXX"} className={"inputt"} onChange={handleChange} /><br /></th>
                         </tr>
                         <tr>
                             <th>Drug_Name</th>
-                            <th className={"inputfield"}><input type={"text"} name={"Drug_Name"} placeholder={"XXXXXXXXXX"} className={"inputt"} onChange={handleChange} /><br/></th>
+                            <th className={"inputfield"}><input type={"text"} name={"Drug_Name"} value={inputs.Drug_Name} placeholder={"XXXXXXXXXX"} className={"inputt"} onChange={handleChange} /><br /></th>
                         </tr>
                         <tr>
                             <th>Category</th>
-                            <th className={"inputfield1"}><input type={"text"} name={"Category"} placeholder={"XXXXXXXXXX"} className={"inputt"} onChange={handleChange} /><br/></th>
+                            <th className={"inputfield1"}><input type={"text"} name={"Category"} value={inputs.Category} placeholder={"XXXXXXXXXX"} className={"inputt"} onChange={handleChange} /><br /></th>
                         </tr>
                         <tr>
                             <th>Dosage</th>
-                            <th className={"inputfield"}><input type={"text"} name={"Drug_dosage"}  placeholder={"XXXmg"} className={"inputt"} onChange={handleChange} /><br/></th>
+                            <th className={"inputfield"}><input type={"text"} name={"Drug_dosage"} value={inputs.Drug_dosage} placeholder={"XXXmg"} className={"inputt"} onChange={handleChange} /><br /></th>
                         </tr>
                         <tr>
                             <th>Description</th>
-                            <th className={"inputfield"}><textarea name={"Descriptions"}  placeholder={"Type description here..."} className={"inputt"} rows={3} onChange={handleChange} /><br/></th>
+                            <th className={"inputfield"}><textarea name={"Descriptions"} value={inputs.Descriptions} placeholder={"Type description here..."} className={"inputt"} rows={3} onChange={handleChange} /><br /></th>
                         </tr>
                     </table>
                 </form>
-
-
-                <hr/>
-
+                <hr />
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="primary" onClick={handleSubmit} >Update</Button>
-                <ToastContainer/>
-                <Button variant="primary" onClick={notify1} >Delete</Button>
-                <ToastContainer/>
+                <Button variant="primary" onClick={handleUpdate}>Update</Button>
+                <ToastContainer />
+                <Button variant="primary" onClick={handleDelete}>Delete</Button>
+                <ToastContainer />
             </Modal.Footer>
         </Modal>
-
     );
 }
 
--- src/components/inventoryinterface/search-section/SearchIDUpdate.js
+++ src/components/inventoryinterface/search-section/SearchIDUpdate.js
@@ -12,15 +12,15 @@ function SearchIDUpdate(props) {
 
     const handleChange = (event) => {
         setSearchTerm(event.target.value);
-    }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
         getUser();
-    }
+    };
 
     function getUser() {
-        axios.get(`http://localhost/HealerZ/PHP/edit.php/${Drug_ID}`)
+        axios.get(`http://localhost/HealerZ/PHP/display.php?Drug_ID=${Drug_ID}`)
             .then(function(response) {
                 console.log(response.data);
                 setInputs(response.data);
@@ -36,39 +36,31 @@ function SearchIDUpdate(props) {
     };
 
     const { show, onHide } = props;
-    return ( 
-        <Modal show = { show }
-        onHide = { onHide }
-        className = { "moddd" } >
-        <Modal.Header closeButton >
-        <Modal.Title> Search </Modal.Title> 
-        </Modal.Header> 
-        < Modal.Body >
-        <div className = { "SearchSection" }
-        style = {
-            { display: 'flex', flexDirection: 'row' } } >
-        <div > <h3 className = { "content-heading1" } > Search DRUG ID: </h3></div >
-        <div className = { "SearchSection3" } >
-        <form onSubmit = { handleSubmit } >
-        <input className = "SearchBox1"
-        type = "text"
-        placeholder = "DRUG_ID"
-        value = { Drug_ID }
-        onChange = { handleChange }
-        /> 
-        <button type = "submit"
-        className = "filterbutt" > Search 
-        </button> 
-        </form> 
-        </div> 
-        </div> {
-            showModal && < UpdateModal show = { showModal }
-            onHide = { UpdateModal1 }
-            inputs = { inputs }
-            />} 
-            </Modal.Body> 
-            </Modal>
-        );
-    }
+    return (
+        <Modal show={show} onHide={onHide} className={"moddd"}>
+            <Modal.Header closeButton>
+                <Modal.Title>Search</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className={"SearchSection"} style={{ display: 'flex', flexDirection: 'row' }}>
+                    <div><h3 className={"content-heading1"}>Search DRUG ID:</h3></div>
+                    <div className={"SearchSection3"}>
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                className="SearchBox1"
+                                type="text"
+                                placeholder="DRUG_ID"
+                                value={Drug_ID}
+                                onChange={handleChange}
+                            />
+                            <button type="submit" className="filterbutt">Search</button>
+                        </form>
+                    </div>
+                </div>
+                {showModal && <UpdateModal show={showModal} onHide={UpdateModal1} inputs={inputs} />}
+            </Modal.Body>
+        </Modal>
+    );
+}
 
-    export default SearchIDUpdate;
+export default SearchIDUpdate;
