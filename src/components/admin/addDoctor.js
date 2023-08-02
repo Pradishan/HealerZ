9575945deac4af7e91f7e615463da42b98ea2355
Jana-JR <janarthanan2skr@gmail.com>
2023-08-03 02:27:21 +0530

    Dash and addDoctor

---
+++ src/components/admin/addDoctor.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import './Admin.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AdminLayout from '../../layouts/AdminLayout';
+import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+function AddPatient(props) {
+    const MySwal = withReactContent(Swal);
+    const [patient_id, setID] = useState('');
+    const [patient_name, setName] = useState('');
+    const [dob, setDob] = useState('');
+    const [gender, setGender] = useState('');
+    const [phoneNo, setphoneNo] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [bg, setBgroup] = useState('');
+    const [pass, setPass] = useState('');
+
+
+
+
+    const handleSubmit = () => {
+        if (patient_id.length === 0) {
+            toast.error("Please Enter the Patient_ID");
+        } else if (patient_name.length === 0) {
+            toast.error("Please Enter the Patient_Name");
+
+        }
+        else if (dob.length === 0) {
+            toast.error("Please Enter the DOB");
+
+        }else  if (gender.length === 0) {
+            toast.error("Please select the Gender");
+
+        }else if (phoneNo.length === 0) {
+            toast.error("Please Enter the PhoneNo");
+
+        } else if (email.length === 0) {
+            toast.error("Please Enter the Email");
+
+        } else if (address.length === 0) {
+            toast.error("Please Enter the Address");
+
+        } else if (bg.length === 0) {
+            toast.error("Please Enter the BloodGroup");
+
+        } else if (pass.length === 0) {
+            toast.error("Please Enter the Password");
+
+        } else {
+            const url = "http://localhost/HealerZ/PHP/addpatient.php";
+            let fdata = new FormData();
+            fdata.append('Patient_ID', patient_id);
+            fdata.append('Patient_Name', patient_name);
+            fdata.append('DateOfBirth', dob);
+            fdata.append('Gender', gender);
+            fdata.append('PhoneNo', phoneNo);
+            fdata.append('Email', email);
+            fdata.append('Address', address);
+            fdata.append('BloodGroup', bg);
+            fdata.append('Password', pass);
+            axios.post(url, fdata)
+            .then((response) => {
+                // Show success swal notification
+                MySwal.fire({
+                    icon: "success",
+                    title: response.data,
+                    customClass: {
+                        container: "sweetalert-container",
+                    },
+                });
+            })
+            .catch((error) => {
+                // Show error swal notification
+                MySwal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: error.message,
+                    customClass: {
+                        container: "sweetalert-container",
+                    },
+                });
+            });
+
+        }
+    }
+    return (
+        <AdminLayout>
+            <div className={"Addcontt"}>
+                    <h2 className="heading-purple">Add Patient</h2>
+                
+                <div className={"addboxx"}>
+                    <form>
+                        <table>
+                            <div style={{ display: "flex", flexDirection: "row" }}>
+                                <div className='cont1'>
+                                    <tr>
+                                        <th><label>Doctor ID:</label></th>
+                                        <th className={"addinputt"}> <input type="text" className="form-control2" name={"Doctor_ID"} placeholder={""} onChange={(e) => setID(e.target.value)} /></th>
+                                    </tr>
+                                    <tr>
+                                        <th> <label>Name:</label></th>
+                                        <th className={"addinputt"}><input type="text" className="form-control2" name={"Doctor_Name"} placeholder={" "} onChange={(e) => setName(e.target.value)} /></th>
+                                    </tr>
+                                    <tr>
+                                        <th> <label>Designation:</label></th>
+                                        <th className={"addinputt"}><input type="text" className="form-control2" name={"Designation"} placeholder={" "} onChange={(e) => setName(e.target.value)} /></th>
+                                    </tr>
+                                    <tr>
+                                        <th> <label>Email:</label></th>
+                                        <th className={"addinputt"}> <input type="text" className="form-control2" name={"Email"} onChange={(e) => setDob(e.target.value)} /></th>
+                                    </tr>
+                                    <tr>
+                                        <th> <label>Phone  No:</label></th>
+                                        <th className={"addinputt"}> <input type="text" className="form-control2" name={"PhoneNo"} placeholder={"076XXXXXXX"} onChange={(e) => setphoneNo(e.target.value)} /></th>
+                                    </tr>
+                                    <tr>
+                                        <th><label>Address:</label></th>
+                                        <th className={"addinputt"}><textarea className={"form-control1"} rows={3} name={"Address"} placeholder={""} onChange={(e) => setAddress(e.target.value)} /></th>
+                                    </tr>
+
+                                    
+                                    <tr>
+                                        <th> <label>Password:</label></th>
+                                        <th className={"addinputt"}> <input type="password" className="form-control2" name={"Password"} placeholder={"Type here"} onChange={(e) => setPass(e.target.value)} /></th>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <th> <label>SLMC Registration No:</label></th>
+                                        <th className={"addinputt"}> <input type="text" className="form-control2" name={"registrationNo"} placeholder={""} onChange={(e) => setPass(e.target.value)} /></th>
+                                    </tr>
+                                </div>
+
+                            </div>
+                        </table>
+                        <hr />
+                    </form>
+                    <button className="btn btn-primary done-button" type="submit" name={"send"} value={"SEND"} onClick={handleSubmit}>ADD</button>
+                    
+                </div>
+                <ToastContainer />
+            </div>
+
+        </AdminLayout>
+
+
+    );
+}
+
+export default AddPatient;
--- src/layouts/AdminLayout.js
+++ src/layouts/AdminLayout.js
@@ -8,9 +8,8 @@ import logos from '../assets/logo-small.png';
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux'
 import { changeToggle } from "../redux/actions";
-import './layout.css';
+import './AdminLayout.css';
 import powsi from "../assets/Powsi.jpg";
-import { NavDropdown } from 'react-bootstrap';
 
 function AdminLayout({ children }) {
 
@@ -30,30 +29,125 @@ function AdminLayout({ children }) {
 
     return (
         <div className="container-fluid">
-            <div className="row flex-nowrap">
-                <div
-                    className={(!open ? " sidebaar col-xl-2" : " w-100px") + " col-auto col-md-1 px-0 bg-white border-right min-vh-100 trans sidebaar"}>
+            
+                
+                {/**  Nav bar */}
+                <div className=" p-0">
+                    <nav className="navbar navbar-expand-lg bg-white shadow fixed-top py-0">
+                <div className="container-fluid d-flex align-items-center">
+                <a
+                    href="/"
+                    className="text-decoration-none color-gradident text-dark fs-5 nav-hover ms-5">
+                </a>
+                <div className="d-flex align-items-center justify-content-center">
+                    {/* logo */}
+                    <img src={logo} alt="HealerZ" height="48px" />
+                </div>
+                {/* right */}
+                <div className="d-flex align-items-center me-3">
+                    {/* notification */}
+                    <div
+                    className="position-relative me-2"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#medicalNotification"
+                    aria-controls="offcanvasExample"
+                    >
+                    <FeatherIcon
+                        icon="bell"
+                        className="icon-hover p-2 fs-1"
+                        height="38px"
+                        width="38px"
+                    />
+
+                    <span className="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-danger fs-7 p-1 mt-1">
+                        99+
+                        <span className="visually-hidden">unread messages</span>
+                    </span>
+                    </div>
+                    {/* profile */}
+                    <div className="dropdown me-5">
+                    <div
+                        className="d-flex align-items-center icon-hover rounded p-2"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        <img
+                        src="https://source.unsplash.com/random/1"
+                        alt="avatar"
+                        height="38px"
+                        width="38px"
+                        className="rounded-circle me-2"
+                        />
+                        <p className="mb-0">Janarthanan</p>
+                    </div>
+
+                    
+                    </div>
+                </div>
+                </div>
+            </nav>
+                    <div>
+                        {children}
+                    </div>
+                </div>
+
+                <div  className='row  mt-5  d-flex  flex-start'>
+                <div col m3>
+
+                {!open ?<div className="logodashboard ms-5">
+                           
+                           <div className='bg-white shadow p-2' >
+                               <div className='m-3'>
+                                   <div className='d-flex align-items-center justify-content-center mb-2'>
+                                       <div className='d-flex align-items-center justify-content-center ms-2'>
+                                           <img src={'https://source.unsplash.com/random/2'} alt='avatar' className='rounded-circle me-2' width='100px' height='100px' />
+                                       </div>
+       
+                                       <div className='d-flex align-items-center justify-content-center'>
+                                           <div>
+                                               <h5 className='m-0'>Janarthanan</h5>
+                                               <p className='fs-8 m-0'>emp20345</p>
+                                               <p className='fs-8 m-0'>admin@std.uwu.ac.lk</p>
+                                               <p className='fs-8 m-0'>0771234567</p>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div> 
+                               </div>:
+                               <div className="logodashboard ms-3">
+                                   <img src={'https://source.unsplash.com/random/2'} alt='avatar' className='rounded-circle me-2' width='50px' height='50px' />
+                               </div>}
+                </div>
+                </div>
+                
+            <div  className='row mt-5'>
+            {/** Side menu */}    
+            
+                <div className="col m-3 mt-4" >
+
+                <div   
+                    className={(!open ? " sidebaar col-xl-2" : " w-100px") + " col-auto col-md-1 bg-transparant px-0 py-0  border-right min-vh-auto trans sidebaar mt-5"  }>
                     <div className={"close-btn-container"} onClick={toggleDrawer}>
                         <img src={SideClose} alt="SideClose" className={!!open && "rotate-180"} />
                     </div>
-                    {!open ? <div className="logodashboard ms-5">
-                        <img src={logo} alt={""} style={{ width: '100px', height: '50px' }} />
-                    </div> :
-                        <div className="logodashboard ms-3">
-                            <img src={logos} alt={""} style={{ width: '50px', height: '50px' }} />
-                        </div>}
-                    <div className={'w-100 border-bottom-d1d1d1 mb-3'} />
+            {/** User card */}       
+                  
+                 {/** END User card */}
+                    <div className={'w-100 border-bottom-d1d1d1'} />
                     <div
-                        className="d-flex flex-column align-items-center align-items-sm-start px-2 pt-2 text-white pt-5">
+                        className="d-flex flex-column align-items-center align-items-sm-start px-2 text-white">
 
 
 
-                        <div className={"w-100 px-sm-2"}>
+                        <div className={"w-100 px-sm-2 bg-white mb-1"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active " : "side-menu-item "}
+                                className={({ isActive }) => isActive ? "menu-item menu-active " : "menu-item "}
                                 to={"/admin/dashboard"}>
                                 <div className={'d-flex'}>
-                                    <FeatherIcon icon="layout" className={!open ? 'me-2' : "ms-1"} />
+                                    <FeatherIcon icon="layout"  className={!open ? 'me-2' : "ms-1"} />
                                     {!open && <div className={'trans-1'}>Dashboard</div>}
                                 </div>
                             </NavLink>
@@ -62,48 +156,70 @@ function AdminLayout({ children }) {
 
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
-                                to={"/doctor"}>
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item" }
+                                to={"/admin/doctor"}>
                                 <div className={'d-flex'}>
-                                    <FeatherIcon icon="briefcase" className={!open ? 'me-2' : "ms-1"} />
+                                    <FeatherIcon icon="activity" className={!open ? 'me-2' : "ms-1"} />    
                                     {!open && <div className={''}>Doctor</div>}
                                 </div>
                             </NavLink>
                         </div>
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
-                                to={"/admin/addpatient"}>
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
+                                to={"/admin/addDoctor"}>
                                 <div className={'d-flex'}>
-                                    <FeatherIcon icon="user-plus" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>ADDPatient</div>}
+                                    <FeatherIcon icon="users" className={!open ? 'me-2' : "ms-1"} />
+                                    {!open && <div className={'trans-1'}>Add Doctor</div>}
                                 </div>
                             </NavLink>
                         </div>
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
+                                to={"/admin/doctorList"}>
+                                <div className={'d-flex'}>
+                                    <FeatherIcon icon="shopping-cart" className={!open ? 'me-2' : "ms-1"} />
+                                    {!open && <div className={'trans-1'}>Doctor List</div>}
+                                </div>
+                            </NavLink>
+                        </div>
+
+                        
+                        <div className={"w-100 px-sm-2"}>
+                            <NavLink
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
+                                to={"/admin/addpatient"}>
+                                <div className={'d-flex'}>
+                                    <FeatherIcon icon="user-plus" className={!open ? 'me-2' : "ms-1"} />
+                                    {!open && <div className={'trans-1'}>ADD Patient</div>}
+                                </div>
+                            </NavLink>
+                        </div>
+                        <div className={"w-100 px-sm-2"}>
+                            <NavLink
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
                                 to={"/admin/editpatient"}>
                                 <div className={'d-flex'}>
                                     <FeatherIcon icon="edit" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>EditPatient</div>}
+                                    {!open && <div className={''}>Edit Patient</div>}
                                 </div>
                             </NavLink>
                         </div>
 
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
                                 to={"/admin/patientlist"}>
                                 <div className={'d-flex'}>
                                     <FeatherIcon icon="list" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>PatientList</div>}
+                                    {!open && <div className={''}>Patient List</div>}
                                 </div>
                             </NavLink>
                         </div>
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
                                 to={"/admin/humanresource"}>
                                 <div className={'d-flex'}>
                                     <FeatherIcon icon="users" className={!open ? 'me-2' : "ms-1"} />
@@ -113,7 +229,7 @@ function AdminLayout({ children }) {
                         </div>
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
                                 to={"/loginInventory"}>
                                 <div className={'d-flex'}>
                                     <FeatherIcon icon="shopping-cart" className={!open ? 'me-2' : "ms-1"} />
@@ -123,7 +239,7 @@ function AdminLayout({ children }) {
                         </div>
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
                                 to={"/admin/medicalreports"}>
                                 <div className={'d-flex'}>
                                     <FeatherIcon icon="layers" className={!open ? 'me-2' : "ms-1"} />
@@ -139,7 +255,7 @@ function AdminLayout({ children }) {
 
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
                                 to={"/inventory-interface/settings"}>
                                 <div className={'d-flex'}>
                                     <FeatherIcon icon="settings" className={!open ? 'me-2' : "ms-1"} />
@@ -150,7 +266,7 @@ function AdminLayout({ children }) {
 
                         <div className={"w-100 px-sm-2"}>
                             <NavLink
-                                className={({ isActive }) => isActive ? "side-menu-item side-menu-active" : "side-menu-item"}
+                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
                                 to={"/loginInventory"}>
                                 <div className={'d-flex'}>
                                     <FeatherIcon icon="log-out" className={!open ? 'me-2' : "ms-1"} />
@@ -162,43 +278,13 @@ function AdminLayout({ children }) {
 
                     </div>
                 </div>
-                <div className="col p-0">
-                    <nav className="navbar navbar-expand-lg bg-white border-bottom-d1d1d1 px-4 navbaar">
-                        <div className="container-fluid">
-                            <div className='panelheading'>
-                                Admin Panel
-                            </div>
-                            <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                                aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                           
-                            <div className="collapse navbar-collapse" id="navbarNav">
-                                <ul className="navbar-nav ms-auto align-items-center">
-                                    <li className="nav-item">
-                                        <a className="nav-link active position-relative px-2" aria-current="page"
-                                            href="#">
-                                            <div className="red-dot" />
-                                            <img src={Bell} alt={""} />
-                                        </a>
-                                    </li>
-                                    <li className="nav-item px-2">
-                                        <a className="nav-link  position-relative p-0" aria-current="page" href="#">
 
-                                            <img src={powsi} alt='avatar' className='rounded-circle me-2' width='40px' height='40px' />
-                                        </a>
-                                    </li>
 
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-                    <div>
-                        {children}
-                    </div>
-                </div>
+
             </div>
+        
+        </div>
+        
 
         </div>
 
