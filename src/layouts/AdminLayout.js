3e2417843e7ce2c611a7e4faa14daef990f8ad5d
Jana-JR <janarthanan2skr@gmail.com>
2023-08-05 20:16:37 +0530

    sidebar change

--- src/layouts/AdminLayout.js
+++ src/layouts/AdminLayout.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import SideClose from "../assets/carbon_side-panel-close.svg";
 import FeatherIcon from 'feather-icons-react';
 import logo from '../assets/logo.png';
@@ -6,26 +7,35 @@ import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux'
 import { changeToggle } from "../redux/actions";
 import Card from 'react-bootstrap/Card';
+import { ProSidebarProvider } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from "react-pro-sidebar";
 import './AdminLayout.css';
+
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import ListIcon from '@mui/icons-material/List';
+import WheelchairPickupIcon from '@mui/icons-material/WheelchairPickup';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+
+
+
 
 function AdminLayout({ children }) {
 
-    const dispatch = useDispatch()
-    const open = useSelector(state => {
-        return state.setting.toggle
-    });
-
-    function toggleDrawer() {
-        dispatch(changeToggle(!open));
-        // setOpen(!open)
-    }
-
-    console.log(open);
-
+    
+    const [collapsed, setCollapsed] = React.useState(false);
 
 
     return (
-        <div className="container-fluid">
+        
+        <div className="container-fluid ">
             
                 
                 {/**  Nav bar */}
@@ -89,199 +99,108 @@ function AdminLayout({ children }) {
                         {children}
                     </div>
                 </div>
-{/** User card */}   
-                <div  className='row  mt-5  d-flex  flex-start'>
-                <div className='col m3'>
 
-                {!open ?<div className="logodashboard ms-5 mt-5">
-                        <Card className=" usercard shadow">
-                        <Card.Body>   
-                               
-                                   <div className='d-flex align-items-center justify-content-center mb-2'>
-                                       <div className='d-flex align-items-center justify-content-center ms-2'>
-                                           <img src={'https://source.unsplash.com/random/2'} alt='avatar' className='rounded-circle me-2' width='80px' height='80px' />
-                                       </div>
-       
-                                       <div className='d-flex align-items-center justify-content-center'>
-                                           <div>
-                                               <h5 className='m-0'>Janarthanan</h5>
-                                               <p className='m-0 text-muted'>emp20345</p>
-                                               <p className='m-0 text-muted'>admin@std.uwu.ac.lk</p>
-                                               <p className='m-0 text-muted'>0771234567</p>
-                                           </div>
-                                       </div>
-                                   </div>
-                               
-                           </Card.Body>
-                       </Card>
-                               </div>:
-                               <div className="logodashboard ms-5 mt-5">
-                                 <Card className="w-100px shadow">
-                                 <Card.Body> 
-                                   <img src={'https://source.unsplash.com/random/2'} alt='avatar' className='rounded-circle me-2' width='50px' height='50px' />
-                                
-                                </Card.Body>
+
+            <div className='col mt-5' style={{ display: "", height: "100vh", backgroundColor: "#00000000"}}>
+            <div  className='row  mt-5  d-flex  flex-start'>
+                            <div className='col m3'>
+
+                            {!collapsed ?<div className="logodashboard ms-5 mt-5">
+                                    <Card className=" usercard shadow">
+                                    <Card.Body>   
+                                        
+                                            <div className='d-flex align-items-center justify-content-center mb-2'>
+                                                <div className='d-flex align-items-center justify-content-center ms-2'>
+                                                    <img src={'https://source.unsplash.com/random/2'} alt='avatar' className='rounded-circle me-2' width='80px' height='80px' />
+                                                </div>
+                
+                                                <div className='d-flex align-items-center justify-content-center'>
+                                                    <div>
+                                                        <h5 className='m-0'>Janarthanan</h5>
+                                                        <p className='m-0 text-muted'>emp20345</p>
+                                                        <p className='m-0 text-muted'>admin@std.uwu.ac.lk</p>
+                                                        <p className='m-0 text-muted'>0771234567</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        
+                                    </Card.Body>
                                 </Card>
-                               </div>}
+                                        </div>:
+                                        <div className="logodashboard ms-5 mt-5">
+                                            <Card className="w-100px shadow">
+                                            <Card.Body> 
+                                            <img src={'https://source.unsplash.com/random/2'} alt='avatar' className='rounded-circle me-2' width='50px' height='50px' />
+                                            
+                                            </Card.Body>
+                                            </Card>
+                                        </div>}
+                            </div>
+                            </div>
+                <Sidebar className="sidebar ms-5 mt-3 row flex-nowarp " collapsed={collapsed}>
+                    <Menu>
+                    <MenuItem>
+                        <button className="sb-button" onClick={() => setCollapsed(!collapsed)}>
+                        {<MenuRoundedIcon  />}
+                        </button>
+                    
+                    
+                    </MenuItem>
+                    
+                    <MenuItem className="menu-item"
+                        component={<Link to="/admin/dashboard"  />}
+                        icon={<GridViewRoundedIcon />}> Dashboard</MenuItem>
+                    
+                    <SubMenu selected className="subline menu-item" label="Doctor" icon={<HealthAndSafetyIcon />} sx={{fontSize:'16px'}} >
+                        <MenuItem  className="menu-item ms-5" sx={{ pl: 0 }}
+                        component={<Link to="/admin/adddoctor"  />}
+                        icon={<PersonAddIcon />}>Add Doctor</MenuItem>
+                        <MenuItem className="menu-item ms-5" sx={{ pl: 0 }}
+                        component={<Link to="/admin/doctorlist"  />}
+                        icon={<ListIcon />}>Doctor List</MenuItem>
+                    </SubMenu>
+                    
+                    <SubMenu className="subline menu-item" label="Patient" icon={<WheelchairPickupIcon />}>
+                        <MenuItem className="menu-item ms-5"
+                        component={<Link to="/admin/addpatient"  />}
+                        icon={<PersonAddAlt1Icon />}>Add Patient</MenuItem>
+                        <MenuItem className="menu-item ms-5"
+                        component={<Link to="/admin/editpatient"  />}
+                        icon={<EditNoteIcon />}>Edit Patient</MenuItem>
+                        <MenuItem className="menu-item ms-5"
+                        component={<Link to="/admin/patientlist"  />}
+                        icon={<ListIcon />}>Patient List</MenuItem>
+                    </SubMenu>
+
+                    <SubMenu className="subline menu-item" label="Human Resource" icon={<Diversity3Icon />}>
+                        <MenuItem className="menu-item ms-5"
+                        component={<Link to="/admin/addemployee" />}
+                        icon={<PersonAddAlt1Icon />}>Add Employee</MenuItem>
+                        <MenuItem className="menu-item ms-5"
+                        component={<Link to="/admin/employeelist"  />}
+                        icon={<ListIcon />}>Employee List</MenuItem>
+                    </SubMenu>
+
+                    <MenuItem className="menu-item"
+                    component={<Link to="/admin/inventory"  />}
+                    icon={<VaccinesIcon />}>Inventory</MenuItem>
+
+                    <MenuItem className="menu-item"
+                    component={<Link to="/admin/medicalreports"  />}
+                    icon={<SummarizeIcon />}>Medical Reports </MenuItem>
+
+                    
+                    <MenuItem className="menu-item" icon={<LogoutRoundedIcon />}> Logout </MenuItem>
+                    </Menu>
+                </Sidebar>
+                <section>
+                    
+                </section>
                 </div>
-                </div>
-{/** END User card */}               
-            <div  className='row mt-1'>
-  
-
-            {/** Side menu */}    
-            
-                <div className="col mt-4" >
-
-                <div   
-                    className={(!open ? " sidebaar col-xl-2" : " w-100px") + " col-auto col-md-1 bg-transparant px-0 py-0  border-right min-vh-auto trans sidebaar ms-5"  }>
-                    <div className={"close-btn-container"} onClick={toggleDrawer}>
-                        <img src={SideClose} alt="SideClose" className={!!open && "rotate-180"} />
-                    </div>
-            
-                    <div className={'w-100 border-bottom-d1d1d1'} />
-                    <div
-                        className="d-flex flex-column align-items-center align-items-sm-start px-2 text-white">
-
-
-
-                        <div className={"w-100 px-sm-2 bg-white mb-1"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active " : "menu-item "}
-                                to={"/admin/dashboard"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="layout"  className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={'trans-1'}>Dashboard</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-
-
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item" }
-                                to={"/doctor"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="activity" className={!open ? 'me-2' : "ms-1"} />    
-                                    {!open && <div className={''}>Doctor</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/admin/adddoctor"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="users" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={'trans-1'}>Add Doctor</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/admin/doctorList"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="shopping-cart" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={'trans-1'}>Doctor List</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-
-                        
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/admin/addpatient"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="user-plus" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={'trans-1'}>ADD Patient</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/admin/editpatient"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="edit" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>Edit Patient</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/admin/patientlist"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="list" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>Patient List</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/admin/humanresource"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="users" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>Human Resource</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/loginInventory"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="shopping-cart" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>Inventory</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/admin/medicalreports"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="layers" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>Medical Reports</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-
-
-
-
-                        <div className={'w-100 border-bottom-d1d1d1 mb-3'} />
-
-                       
-
-                        <div className={"w-100 px-sm-2"}>
-                            <NavLink
-                                className={({ isActive }) => isActive ? "menu-item menu-active" : "menu-item"}
-                                to={"/loginInventory"}>
-                                <div className={'d-flex'}>
-                                    <FeatherIcon icon="log-out" className={!open ? 'me-2' : "ms-1"} />
-                                    {!open && <div className={''}>Logout</div>}
-                                </div>
-                            </NavLink>
-                        </div>
-
-
-                    </div>
-                </div>
-
-
-
-            </div>
-        
-        </div>
-        
+       
 
         </div>
-
+    
 
     );
 }
