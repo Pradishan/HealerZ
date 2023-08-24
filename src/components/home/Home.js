502e58760795a11c6d1d5a020f9d0c5d8984d833
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-08-24 12:45:05 +0530

    home page css chnaged

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,14 @@ import Hcard from './Hcard';
 import Hteam from './Hteam';
 import logo from '../../assets/logo.png';
 import { Icon } from '@iconify/react';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './Home.css';
+import FeatherIcon from 'feather-icons-react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
 
 
@@ -24,12 +31,12 @@ export default function Home() {
         <Carousel />
       </div>
 
-      
+
       <div className='services' id='services'>
-      <h1 className='serhed'>Healerz</h1>
-     
+        <h1 className='serhed'>Healerz</h1>
+
         <div className="container text-center mt-2">
-          
+
           <h1 className='serhed'>OUR SERVICES</h1>
           <div className="container text-center mt-2 contgap1">
             <div className="row  cardcontainer1">
@@ -51,14 +58,14 @@ export default function Home() {
                   <h4> Doctor-Patient Collaboration </h4>
                 </div>
               </div>
-             
+
             </div>
-           
+
           </div>
         </div>
       </div>
-      
-     
+
+
 
       <div className="container" id='aboutus'>
         <div class="row mt-5 p-5 row1">
@@ -81,23 +88,89 @@ export default function Home() {
           </div>
         </div>
       </div>
-    
+
 
       <div className="events">
         <Hcard />
       </div>
 
-      <div className="Hfooter bottom">
-      <footer bgColor='light' className='text-center text-lg-left'>
-          <div className='text-center p-3' style={{ backgroundColor: '#fff' }}>
-          <a className='text-dark' href='https://HealerZ.com/'>
-          <img src={logo} alt="HealerZ" height='30px' />
-           </a>
-            &copy; {new Date().getFullYear()} Copyright. All rights reserved.{' '}
-            
-          </div>
-        </footer>
-      </div>
+      <MDBFooter className='bg-light text-center'>
+        <MDBContainer className='p-4 pb-0'>
+          <section className='mb-4'>
+            <MDBBtn
+              floating
+              className='m-1 sosicon'
+              style={{ backgroundColor: '#3b5998' }}
+              href='#!'
+              role='button'
+            >
+              <FeatherIcon icon="facebook" className='sociccon' />
+            </MDBBtn>
+
+            <MDBBtn
+              floating
+              className='m-1 sosicon'
+              style={{ backgroundColor: '#55acee' }}
+              href='#!'
+              role='button'
+            >
+              <FeatherIcon icon="twitter" className='sociccon' />
+            </MDBBtn>
+
+            <MDBBtn
+              floating
+              className='m-1 sosicon'
+              style={{ backgroundColor: '#dd4b39' }}
+              href='#!'
+              role='button'
+            >
+              <FeatherIcon icon="mail" className='sociccon' />
+            </MDBBtn>
+            <MDBBtn
+              floating
+              className='m-1 sosicon'
+              style={{ backgroundColor: '#ac2bac' }}
+              href='#!'
+              role='button'
+            >
+              <FeatherIcon icon="instagram" className='sociccon' />
+            </MDBBtn>
+
+            <MDBBtn
+              floating
+              className='m-1 sosicon'
+              style={{ backgroundColor: '#0082ca' }}
+              href='#!'
+              role='button'
+            >
+              <FeatherIcon icon="linkedin" className='sociccon' />
+            </MDBBtn>
+
+            <MDBBtn
+              floating
+              className='m-1 sosicon'
+              style={{ backgroundColor: '#333333' }}
+              href='#!'
+              role='button'
+            >
+              <FeatherIcon icon="github" className='sociccon' />
+            </MDBBtn>
+          </section>
+        </MDBContainer>
+        <hr style={{width:'350px',marginLeft:'500px'}}/>
+        
+        <a className='text-dark' href='https://HealerZ.com/'>
+            <img src={logo} alt="HealerZ" height='50px' />
+          </a>
+          <hr />
+        <div className='text-center ' style={{ backgroundColor: '#fff', marginBottom: '10px' }}>
+         
+          &copy; {new Date().getFullYear()} Copyright. All rights reserved for UWU{' '}
+
+        </div>
+      </MDBFooter>
     </div>
+
+
   )
 }
