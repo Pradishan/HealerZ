01376e63df426c9011eb9cfea61c3d9ccf1ccf17
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-09-02 12:03:01 +0530

    backend for admin part

--- src/components/admin/AddEmployee.js
+++ src/components/admin/AddEmployee.js
@@ -4,78 +4,88 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AdminLayout from "../../layouts/AdminLayout";
 import axios from "axios";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
 
-function AddDoctor(props) {
-  const MySwal = withReactContent(Swal);
-  const [doctor_id, setID] = useState("");
-  const [doctor_name, setName] = useState("");
-  const [phoneNo, setphoneNo] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  const [pass, setPass] = useState("");
-  const [image, setImage] = useState("");
-  const [regNo, setReg] = useState("");
+function AddEmployee(props) {
+  const [pharmacist_id, setID] = useState('');
+  const [pharmacist_name, setName] = useState('');
+  const [phoneNo, setphoneNo] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [pass, setPass] = useState('');
+  const [image, setImage] = useState('');
+  const [regNo, setReg] = useState('');
+  const [designation, setDesignation] = useState('');
+
+  const resetForm = () => {
+    setID('');
+    setName('');
+    setphoneNo('');
+    setEmail('');
+    setAddress('');
+    setPass('');
+    setImage('');
+    setReg('');
+    setDesignation('');
+}
 
   const handleSubmit = () => {
-    // if (patient_id.length === 0) {
-    //     toast.error("Please Enter the Patient_ID");
-    // } else if (patient_name.length === 0) {
-    //     toast.error("Please Enter the Patient_Name");
-
-    // }
-    // else if (dob.length === 0) {
-    //     toast.error("Please Enter the DOB");
-
-    // }else
-    if (phoneNo.length === 0) {
-      toast.error("Please Enter the PhoneNo");
+    if (pharmacist_id.length === 0) {
+      toast.warning("Please Enter the Pharmacist_ID");
+    } else if (pharmacist_name.length === 0) {
+      toast.warning("Please Enter the Pharmacist_Name");
+    } else if (designation.length === 0) {
+      toast.warning("Please Enter the Designation");
     } else if (email.length === 0) {
-      toast.error("Please Enter the Email");
+      toast.warning("Please Enter the Email");
+    } else if (phoneNo.length === 0) {
+      toast.warning("Please Enter the PhoneNo");
     } else if (address.length === 0) {
-      toast.error("Please Enter the Address");
+      toast.warning("Please Enter the Address");
     } else if (pass.length === 0) {
-      toast.error("Please Enter the Password");
+      toast.warning("Please Enter the Password");
+    } else if (regNo.length === 0) {
+      toast.warning("Please Enter the SLMC Registration No");
+    } else if (image.length === 0) {
+      toast.warning("Please Import the Image");
     } else {
-      const url = "http://localhost/HealerZ/PHP/addpatient.php";
+      const url = "http://localhost/HealerZ/PHP/admin/addEmployee.php";
       let fdata = new FormData();
-      fdata.append("doctor_ID", doctor_id);
-      fdata.append("doctor_Name", doctor_name);
-      fdata.append("PhoneNo", phoneNo);
-      fdata.append("Email", email);
-      fdata.append("Address", address);
-      fdata.append("Password", pass);
-      fdata.append("regNo", regNo);
-      fdata.append("imageupload", image);
-      axios
-        .post(url, fdata)
-        .then((response) => {
-          // Show success swal notification
-          MySwal.fire({
-            icon: "success",
-            title: response.data,
-            customClass: {
-              container: "sweetalert-container",
-            },
-          });
-        })
-        .catch((error) => {
-          // Show error swal notification
-          MySwal.fire({
-            icon: "error",
-            title: "Error",
-            text: error.message,
-            customClass: {
-              container: "sweetalert-container",
-            },
-          });
-        });
-    }
+            fdata.append('pharmacist_ID', pharmacist_id);
+            fdata.append('pharmacist_name', pharmacist_name);
+            fdata.append('designation', designation);
+            fdata.append('email', email);
+            fdata.append('phoneNo', phoneNo);
+            fdata.append('address', address);
+            fdata.append('password', pass);
+            fdata.append('regNo', regNo);
+            fdata.append('imageUpload', image);
+            axios.post(url, fdata)
+            .then((response) => {
+                if (response.data.message === "Pharmacist Added Successfully") {
+                    // Show success message
+                    toast.success(response.data.message);
+                    resetForm();
+                } else {
+                    // Show error message
+                    toast.error("Pharmacist Already Added");
+                }
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            });
+          }
+
+        
   };
   return (
     <AdminLayout>
+      {/* <div className={ "addboxx mt-4" } style={ { display: "flex", justifyContent: "center", alignItems: "center" } }  > */}
       <div className={"Addcontt "}>
+        {/* <div class="card-body">
+                        <a class="btn btn-primary">Add Doctor</a>
+                    </div>
+
+                    <hr /> */}
         <h3 className="serhett">Add Pharmacist</h3>
         <div className={"addboxx"}>
           <form>
@@ -91,9 +101,10 @@ function AddDoctor(props) {
                       <input
                         type="text"
                         className="form-control1"
-                        name={"doctor_ID"}
-                        placeholder={"PHDXXXXX"}
+                        name={"pharmacist_ID"}
+                        placeholder={"DocXXXXX"}
                         onChange={(e) => setID(e.target.value)}
+                        value={pharmacist_id}
                       />
                     </th>
                   </tr>
@@ -106,9 +117,10 @@ function AddDoctor(props) {
                       <input
                         type="text"
                         className="form-control1"
-                        name={"doctor_Name"}
-                        placeholder={"Jana"}
+                        name={"pharmacist_name"}
+                        placeholder={"Janarthanan"}
                         onChange={(e) => setName(e.target.value)}
+                        value={pharmacist_name}
                       />
                     </th>
                   </tr>
@@ -122,8 +134,9 @@ function AddDoctor(props) {
                         type="text"
                         className="form-control1"
                         name={"designation"}
-                        placeholder={"DSDXXXXX"}
-                        onChange={(e) => setName(e.target.value)}
+                        placeholder={"XXXXXX"}
+                        onChange={(e) => setDesignation(e.target.value)}
+                        value={designation}
                       />
                     </th>
                   </tr>
@@ -141,6 +154,7 @@ function AddDoctor(props) {
                         name={"email"}
                         placeholder={"Jana343@gmail.com"}
                         onChange={(e) => setEmail(e.target.value)}
+                        value={email}
                       />
                     </th>
                   </tr>
@@ -157,6 +171,7 @@ function AddDoctor(props) {
                         name={"phoneNo"}
                         placeholder={"076XXXXXXX"}
                         onChange={(e) => setphoneNo(e.target.value)}
+                        value={phoneNo}
                       />
                     </th>
                   </tr>
@@ -171,9 +186,10 @@ function AddDoctor(props) {
                       <textarea
                         className={"form-control1"}
                         rows={3}
-                        name={"Address"}
-                        placeholder={"Type here..."}
+                        name={"address"}
+                        placeholder={"Type here...."}
                         onChange={(e) => setAddress(e.target.value)}
+                        value={address}
                       />
                     </th>
                   </tr>
@@ -188,9 +204,10 @@ function AddDoctor(props) {
                       <input
                         type="password"
                         className="form-control1"
-                        name={"Password"}
-                        placeholder={"Type Password here..."}
+                        name={"password"}
+                        placeholder={"Type password here"}
                         onChange={(e) => setPass(e.target.value)}
+                        value={pass}
                       />
                     </th>
                   </tr>
@@ -206,8 +223,9 @@ function AddDoctor(props) {
                         type="text"
                         className="form-control1"
                         name={"regNo"}
-                        placeholder={"SLMCXXXX"}
+                        placeholder={"SMDXXXXX"}
                         onChange={(e) => setReg(e.target.value)}
+                        value={regNo}
                       />
                     </th>
                   </tr>
@@ -224,6 +242,7 @@ function AddDoctor(props) {
                         name={"imageUpload"}
                         placeholder={"choose file"}
                         onChange={(e) => setImage(e.target.files)}
+                        value={image}
                       />
                     </th>
                   </tr>
@@ -231,21 +250,24 @@ function AddDoctor(props) {
               </div>
             </table>
             <hr />
-            <button
-              className="btn btn-primary done-button"
-              style={{ left: "700px" }}
-              type="submit"
-              name={"send"}
-              value={"SEND"}
-              onClick={handleSubmit}
-            >
-              ADD
-            </button>
           </form>
+          <button
+            className="btn btn-primary done-button"
+            type="submit"
+            name={"send"}
+            value={"SEND"}
+            onClick={handleSubmit}
+          >
+            ADD
+          </button>
+          <button className="btn btn-secondary done-buttontt3" onClick={resetForm}>Reset</button>
         </div>
+        <ToastContainer />
       </div>
+
+      {/* </div> */}
     </AdminLayout>
   );
 }
 
-export default AddDoctor;
+export default AddEmployee;
