d96fbcc8f48c005a1017c599f05555ffd857b541
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-09-02 17:59:57 +0530

    Update modal changes

--- src/components/inventoryinterface/modals/UpdateModal.js
+++ src/components/inventoryinterface/modals/UpdateModal.js
@@ -1,35 +1,34 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
-import "../inventory.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
 function UpdateModal(props) {
+  const { show, onHide, inputs } = props;
   const [newData, setNewData] = useState({});
+  const item = inputs[0] || {}; // Access the first item in the array or an empty object
 
-  const updateNewData = (e, field) => {
+  const [drug_id, setID] = useState(item.Drug_ID);
+  const [drug_name, setName] = useState(item.Drug_Name);
+  const [category, setCat] = useState(item.Category);
+  const [dosage, setDos] = useState(item.Drug_dosage);
+  const [description, setDes] = useState(item.Descriptions);
+
+  useEffect(() => {
     setNewData({
-      ...newData,
-      [field]: e.target.value,
+      Drug_ID: drug_id,
+      Drug_Name: drug_name,
+      Category: category,
+      Drug_dosage: dosage,
+      Descriptions: description,
     });
-  };
-
-  const { show, onHide } = props;
-
-  const items = props.inputs; 
+  }, [drug_id, drug_name, category, dosage, description]);
 
   const handleUpdate = () => {
-    if (Object.keys(newData).length === 0) {
-      toast.info("No data to update!");
-      return;
-    }
-    if (!newData.Drug_ID) {
-      toast.info("Drug ID is missing in the update data!");
-      return;
-    }
+   
     axios
-    .put("http://localhost/HealerZ/PHP/Inventory/updateDrug.php", newData)
+      .put("http://localhost/HealerZ/PHP/Inventory/updateDrug.php", newData)
       .then((response) => {
         console.log(response.data);
         toast.success("Drug updated successfully!");
@@ -40,28 +39,26 @@ function UpdateModal(props) {
         console.error(error);
       });
   };
-  
-  
-  
+
   const handleDelete = () => {
-    const drugIdToDelete = newData.Drug_ID || (items.length && items[0].Drug_ID);
+    const drugIdToDelete = newData.Drug_ID || item.Drug_ID;
 
     if (!drugIdToDelete) {
       toast.error("Drug ID not found!");
       return;
     }
 
-      axios
-        .delete(`http://localhost/HealerZ/PHP/Inventory/deleteDrug.php?Drug_ID=${drugIdToDelete}`)
-        .then((response) => {
-          console.log(response.data);
-          toast.success("Drug deleted successfully!");
-          onHide();
-        })
-        .catch((error) => {
-          toast.error("Failed to delete drug!");
-          console.error(error);
-        });
+    axios
+      .delete(`http://localhost/HealerZ/PHP/Inventory/deleteDrug.php?Drug_ID=${drugIdToDelete}`)
+      .then((response) => {
+        console.log(response.data);
+        toast.success("Drug deleted successfully!");
+        onHide();
+      })
+      .catch((error) => {
+        toast.error("Failed to delete drug!");
+        console.error(error);
+      });
   };
 
   return (
@@ -74,91 +71,89 @@ function UpdateModal(props) {
         <form>
           <table className={"ADDTable"}>
             <tbody>
-              {items.map((item) => (
-                <React.Fragment key={item.Drug_ID}>
-                  <tr>
-                    <th>Drug_ID</th>
-                    <th className={"inputfield"}>
-                      <input
-                        type={"text"}
-                        name={"Drug_ID"}
-                        defaultValue={item.Drug_ID}
-                        placeholder={"DRUGXXXXXX"}
-                        className={"inputt"}
-                        onChange={(e) => updateNewData(e, "Drug_ID")}
-                      />
-                      <br />
-                    </th>
-                  </tr>
+              <tr>
+                <th>Drug_ID</th>
+                <th className={"inputfield"}>
+                  <input
+                    type={"text"}
+                    name={"Drug_ID"}
+                    defaultValue={drug_id}
+                    placeholder={"DRUGXXXXXX"}
+                    className={"inputt"}
+                    onChange={(e) => setID(e.target.value)}
+                  />
+                  <br />
+                </th>
+              </tr>
 
-                  <tr>
-                    <th>Drug_Name</th>
-                    <th className={"inputfield"}>
-                      <input
-                        type={"text"}
-                        name={"Drug_Name"}
-                        defaultValue={item.Drug_Name}
-                        placeholder={"XXXXXXXXXX"}
-                        className={"inputt"}
-                        onChange={(e) => updateNewData(e, "Drug_Name")}
-                      />
-                      <br />
-                    </th>
-                  </tr>
-              
-                  <tr>
-                            <th>Category</th>
-                            <th className={'inputfield'}>
-                                <select
-                                    name={'Category'}
-                                    defaultValue={item.Category}
-                                    className={'inputt'}
-                                    onChange={(e) => updateNewData(e, "Category")}
-                                    style={{height:'30px '}}
-                                >
-                                   <option value={''}>Select Category</option>
-                                    <option value={'Liquid'}>Liquid</option>
-                                    <option value={'Tablet'}>Tablet</option>
-                                    <option value={'Capsules'}>Capsules</option>
-                                    <option value={'Topical'}>Topical</option>
-                                    <option value={'Suppositories'}>Suppositories</option>
-                                    <option value={'Drops'}>Drops</option>
-                                    <option value={'Injections'}>Injections</option>
-                                    <option value={'Implants'}>Implants</option>
-                                </select>
-                                <br />
-                            </th>
-                        </tr>
-                  <tr>
-                    <th>Dosage</th>
-                    <th className={"inputfield"}>
-                      <input
-                        type={"text"}
-                        name={"Drug_dosage"}
-                        defaultValue={item.Drug_dosage}
-                        placeholder={"XXXmg"}
-                        className={"inputt"}
-                        onChange={(e) => updateNewData(e, "Drug_dosage")}
-                      />
-                      <br />
-                    </th>
-                  </tr>
-                  <tr>
-                    <th>Description</th>
-                    <th className={"inputfield"}>
-                      <textarea
-                        name={"Descriptions"}
-                        defaultValue={item.Descriptions}
-                        placeholder={"Type description here..."}
-                        className={"inputt"}
-                        rows={3}
-                        onChange={(e) => updateNewData(e, "Descriptions")}
-                      />
-                      <br />
-                    </th>
-                  </tr>
-                </React.Fragment>
-              ))}
+              <tr>
+                <th>Drug_Name</th>
+                <th className={"inputfield"}>
+                  <input
+                    type={"text"}
+                    name={"Drug_Name"}
+                    defaultValue={drug_name}
+                    placeholder={"XXXXXXXXXX"}
+                    className={"inputt"}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <br />
+                </th>
+              </tr>
+
+              <tr>
+                <th>Category</th>
+                <th className={"inputfield"}>
+                  <select
+                    name={"Category"}
+                    defaultValue={category}
+                    className={"inputt"}
+                    onChange={(e) => setCat(e.target.value)}
+                    style={{ height: "30px " }}
+                  >
+                    <option value={""}>Select Category</option>
+                    <option value={"Liquid"}>Liquid</option>
+                    <option value={"Tablet"}>Tablet</option>
+                    <option value={"Capsules"}>Capsules</option>
+                    <option value={"Topical"}>Topical</option>
+                    <option value={"Suppositories"}>Suppositories</option>
+                    <option value={"Drops"}>Drops</option>
+                    <option value={"Injections"}>Injections</option>
+                    <option value={"Implants"}>Implants</option>
+                  </select>
+                  <br />
+                </th>
+              </tr>
+
+              <tr>
+                <th>Dosage</th>
+                <th className={"inputfield"}>
+                  <input
+                    type={"text"}
+                    name={"Drug_dosage"}
+                    defaultValue={dosage}
+                    placeholder={"XXXmg"}
+                    className={"inputt"}
+                    onChange={(e) => setDos(e.target.value)}
+                  />
+                  <br />
+                </th>
+              </tr>
+
+              <tr>
+                <th>Description</th>
+                <th className={"inputfield"}>
+                  <textarea
+                    name={"Descriptions"}
+                    defaultValue={description}
+                    placeholder={"Type description here..."}
+                    className={"inputt"}
+                    rows={3}
+                    onChange={(e) => setDes(e.target.value)}
+                  />
+                  <br />
+                </th>
+              </tr>
             </tbody>
           </table>
         </form>
@@ -166,10 +161,18 @@ function UpdateModal(props) {
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="primary uptbut" onClick={handleUpdate} style={{backgroundColor:'green'}}>
+        <Button
+          variant="primary uptbut"
+          onClick={handleUpdate}
+          style={{ backgroundColor: "green" }}
+        >
           Update
         </Button>
-        <Button variant="primary uptbut" onClick={handleDelete} style={{backgroundColor:'red'}}>
+        <Button
+          variant="primary uptbut"
+          onClick={handleDelete}
+          style={{ backgroundColor: "red" }}
+        >
           Delete
         </Button>
         <ToastContainer />
