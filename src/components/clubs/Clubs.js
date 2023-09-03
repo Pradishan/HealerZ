918bc9d50aeef8450c8b6d40552fa1241d70f517
mohamednusnan <nusnanam12@gmail.com>
2023-09-03 20:41:11 +0530

    Merge branch 'main' of https://github.com/mohamednusnan/HealerZ

--- src/components/clubs/Clubs.js
+++ src/components/clubs/Clubs.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ClubLayout from '../../layouts/ClubLayout';
 import Card from 'react-bootstrap/Card';
-import HbloodReg from '../home/HbloodReg';
+import Eventslider  from './Eventslider';
 import '../admin/Admin.css'
-import { useState } from 'react';
+
 
 export default function Clubs() {
-  const [show, setShow] = useState(false);
-  const handleShow = () => setShow(true);
+
   return (
      <ClubLayout>
         <div className="col flex-nowrap" >
@@ -36,34 +35,13 @@ export default function Clubs() {
                 </div>        
            </div>
         </div>
-
-        <div className="col flex-nowrap" >
-          <div className={"outerbox d-flex mt-4 flex-md-column"}>
-             <div className="StockIndicator d-flex flex-row justify-content-around" >
-                  <Card className="card1" style={{backgroundColor: 'white' }}>
-                      <Card.Body>
-                          <Card.Title className="CardTitle d-flex justify-content-around">Vaccination </Card.Title>
-                            <div className="card2 d-flex justify-content-center">
-                            <HbloodReg onClick={handleShow}></HbloodReg>
-                            </div>
-                           </Card.Body>
-                       </Card>
-                   
-                 
-                       <Card className="card1">
-                           <Card.Body>
-                               <Card.Title className="CardTitle">Blood Donation</Card.Title>
-                               <div className="card2 d-flex justify-content-center">
-                               <HbloodReg onClick={handleShow}></HbloodReg>
-                               </div>
-                           </Card.Body>
-                       </Card>         
-                                
-                </div>        
-           </div>
-        </div>
+<div className='container text-center '>
+  <div className='row mt-3 justify-content-center'>
+     <Eventslider/>
+    </div>
+  </div>
     
-     </ClubLayout>
+  </ClubLayout>
 
   )
 }
