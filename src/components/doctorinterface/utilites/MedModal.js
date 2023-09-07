1ad15af4eee4e3b58efb6d0afc4c28ff9aca7a37
Pradisharoon <90965355+Pradishan@users.noreply.github.com>
2023-09-07 20:46:03 +0530

    select id error fixed i think

--- src/components/doctorinterface/utilites/MedModal.js
+++ src/components/doctorinterface/utilites/MedModal.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Modal } from "react-bootstrap";
+import { Modal} from "react-bootstrap";
+
+
 
 export default function MedModal ( props )
 {
     const { show, onHide, id } = props;
-    const [ records, setRecords ] =  useState([]);
+    const [ records, setRecords ] = useState( [] );
 
     useEffect( () =>
     {
@@ -21,20 +23,20 @@ export default function MedModal ( props )
             }
         };
         fetchData();
-    }, [ id ] ); 
+    }, [ id ] );
 
     return (
         <Modal size='xl' show={ show } onHide={ onHide } backdrop="static" keyboard={ false }>
             <Modal.Header closeButton>
-                <Modal.Title>Medical Records | {id}</Modal.Title>
+                <Modal.Title>Medical Records | { id }</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <div className="row">
+            <div className="row">
                     <div className="col-3" style={ { maxHeight: '70vh', overflow: 'auto' } }>
                         <nav id="datenavbar" className="h-10 flex-column align-items-stretch pe-4 border-end">
                             <nav className="nav nav-pills flex-column">
                                 { Array.isArray(records) ? (records.map( ( record ) => (
-                                    <a key={ record.DateandTime } className={ `nav-link` } href={ `#${ record.DateandTime }` }>{ record.DateandTime }</a>
+                                    <a key={ record.DateandTime } className={ `nav-link` } href={ `#${ record.MedicalRecord_ID }` }>{ record.DateandTime }</a>
                                 ) )) : (
                                     <p>No records to display.</p>
                                   ) }
@@ -44,7 +46,7 @@ export default function MedModal ( props )
                     <div className="col-9" style={ { maxHeight: '70vh', overflow: 'auto' } }>
                         <div data-bs-spy="scroll" data-bs-target="#datenavbar" data-bs-offset="0" className="scrollspy-example-2" tabIndex="0">
                             { Array.isArray(records) ? (records.map( ( record ) => (
-                                <div key={ record.DateandTime } id={ record.DateandTime }>
+                                <div key={ record.MedicalRecord_ID } id={ record.MedicalRecord_ID }>
                                     <p><span className='fw-bold'>Date : </span>{ record.DateandTime }</p>
                                     <h5>Patient complain</h5>
                                     <p>{ record.Patientcomplaint }</p>
