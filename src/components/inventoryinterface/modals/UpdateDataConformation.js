04f3d3fa38908b0e33223bc970900ad0185adcbf
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-09-09 21:38:04 +0530

    Update Details Conformation added

--- src/components/inventoryinterface/modals/UpdateDataConformation.js
+++ src/components/inventoryinterface/modals/UpdateDataConformation.js
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Modal, Button} from "react-bootstrap"; // You can use any modal library you prefer
 
-const UpdateConfirmModal = ({ show, onHide, onConfirm }) => {
+const UpdateDataConformation = ({ show, onHide, onConfirm }) => {
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header>
-        <Modal.Title className="modaltitleee1" style={{color:'Green'}}>Conformation to Update Stock</Modal.Title>
+        <Modal.Title className="modaltitleee1" style={{color:'Green'}}>Conformation to Update Details</Modal.Title>
       </Modal.Header>
       <Modal.Body style={{ background: "white" }}>
-        <p>Are you sure you want to Update this Stock</p>
+        <p>Are you sure you want to Update this Stock Details</p>
       </Modal.Body>
       <Modal.Footer>
       <Button variant="success" style={{width:'200px'}} onClick={onConfirm}>
@@ -24,10 +24,10 @@ const UpdateConfirmModal = ({ show, onHide, onConfirm }) => {
   );
 };
 
-UpdateConfirmModal.propTypes = {
+UpdateDataConformation.propTypes = {
   show: PropTypes.bool.isRequired,
   onHide: PropTypes.func.isRequired,
   onConfirm: PropTypes.func.isRequired,
 };
 
-export default UpdateConfirmModal;
+export default UpdateDataConformation;
