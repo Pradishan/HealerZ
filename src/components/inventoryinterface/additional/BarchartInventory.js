e6440b5c6a63ac1ff7a95eaa5c4ce9d94f6591a3
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-10-07 20:46:33 +0530

    some changes in barchart

--- src/components/inventoryinterface/additional/BarchartInventory.js
+++ src/components/inventoryinterface/additional/BarchartInventory.js
@@ -7,7 +7,12 @@ class BarchartInventory extends Component {
     super(props);
 
     this.state = {
-      series: [],
+      series: [
+        {
+          name: 'Category Count',
+          data: [],
+        },
+      ],
       options: {
         chart: {
           height: 200,
@@ -24,12 +29,12 @@ class BarchartInventory extends Component {
         dataLabels: {
           enabled: true,
           formatter: function (val) {
-            return val.toFixed(2) + "%";
+            return val.toFixed(2) + '%';
           },
           offsetY: -20,
           style: {
             fontSize: '12px',
-            colors: ["#304758"],
+            colors: ['#304758'],
           },
         },
         xaxis: {
@@ -66,8 +71,28 @@ class BarchartInventory extends Component {
           },
           labels: {
             show: false,
-            formatter: function (val) {
-                return val.toFixed(2) + "%";
+            formatter: (val, opts) => {
+              if (opts && typeof opts.dataPointIndex !== 'undefined') {
+                const { categories } = this.state.options.xaxis;
+                if (categories[opts.dataPointIndex] === 'Tablet') {
+                  return val.toFixed(2) + '% HighStock: 800, LowStock: 100';
+                } else if (categories[opts.dataPointIndex] === 'Drops') {
+                  return val.toFixed(2) + '% HighStock: 300, LowStock: 50';
+                } else if (categories[opts.dataPointIndex] === 'Liquid') {
+                  return val.toFixed(2) + '% HighStock: 600, LowStock: 70';
+                } else if (categories[opts.dataPointIndex] === 'Capsules') {
+                  return val.toFixed(2) + '% HighStock: 700, LowStock: 100';
+                } else if (categories[opts.dataPointIndex] === 'Topical') {
+                  return val.toFixed(2) + '% HighStock: 200, LowStock: 50';
+                } else if (categories[opts.dataPointIndex] === 'Suppositories') {
+                  return val.toFixed(2) + '% HighStock: 200, LowStock: 50';
+                } else if (categories[opts.dataPointIndex] === 'Injections') {
+                  return val.toFixed(2) + '% HighStock: 300, LowStock: 30';
+                }else if (categories[opts.dataPointIndex] === 'Implants') {
+                  return val.toFixed(2) + '% HighStock: 150, LowStock: 20';
+                }
+              }
+              return val.toFixed(2) + '%';
             },
           },
         },
@@ -90,15 +115,12 @@ class BarchartInventory extends Component {
       .then((response) => {
         const data = response.data;
         const categories = data.map((item) => item.Category);
-        const percentages = data.map((item) => item.percentage); 
+        const percentages = data.map((item) => item.percentage);
 
-        console.log(categories);
-        console.log(percentages);
-        
         this.setState({
           series: [
             {
-              name: 'Category Count',
+              name: 'Category Percentage',
               data: percentages,
             },
           ],
