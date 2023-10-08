3c5920d6bffa5f57a8b7d8de7531dfb283a72896
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-10-08 23:41:21 +0530

    chanes

--- PHP/Inventory/deletePrescription.php
+++ PHP/Inventory/deletePrescription.php
@@ -1,41 +1,4 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header("Content-Type: application/json; charset=UTF-8");
-
-// // Include your database connection code here
-// // Replace these with your database credentials
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "healerz"; // Your database name
-
-// // Create a connection to the database
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// // Check the connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-// // Get the Prescription_ID from the URL parameter
-// $prescription_id = $_GET['id'];
-
-// // Prepare and execute the SQL query to delete the prescription
-// $sql = "DELETE FROM prescription_record WHERE Prescription_ID = ?";
-// $stmt = $conn->prepare($sql);
-// $stmt->bind_param("s", $prescription_id);
-
-// if ($stmt->execute()) {
-//     echo json_encode(array("message" => "Prescription deleted successfully"));
-// } else {
-//     echo json_encode(array("message" => "Error deleting prescription"));
-// }
-
-// $stmt->close();
-// $conn->close();
-
-
-
 header("Access-Control-Allow-Origin: http://localhost:3000"); 
 header("Access-Control-Allow-Methods: DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
