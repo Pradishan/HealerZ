071404f5ca16edf3252faf2feb246d9b6b45efa1
Powsihan Indrakumar <powsipowsihan07@gmail.com>
2023-10-09 10:51:31 +0530

    admin dashboard changes

--- src/components/admin/DashboardAdmin.js
+++ src/components/admin/DashboardAdmin.js
@@ -1,34 +1,25 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import AdminLayout from "../../layouts/AdminLayout";
 import "./Admin.css";
-// import nusnan from "../../assets/nusnan.jpg";
-// import pradi from "../../assets/pradi.jpg";
-// import thanu from "../../assets/thanu.jpg";
-// import powsi from "../../assets/Powsi.jpg";
-// import farhath from "../../assets/farhath.jpg";
-// import jana from "../../assets/jana.jpg";
-// import joshi from "../../assets/joshi.jpg";
-import BarChart from "./additional/Barchart";
-
+import BarchartDrugsoutcome from "../inventoryinterface/additional/BarchartDrugsoutcome";
+import Calendar from "react-calendar";
 
 export default function DashboardAdmin() {
+  const [value, onChange] = useState(new Date());
 
   return (
     <AdminLayout>
-      <div className="barchartcontainer">
-      <BarChart/>
+      <div className="dashboradmin" style={{ marginTop: "20px" }}>
+        <div className="catbarchartt">
+          <BarchartDrugsoutcome />
+          <hr />
+          <Calendar
+            onChange={onChange}
+            value={value}
+            className="custom-calendar7"
+          />
+        </div>
       </div>
-      
-      {/* <div className="container text-center mu-auto teamele teaaaaam">
-        <h3 className="serhett">MEET OUR TEAM</h3>
-        <img src={nusnan} className="card-img-top img" alt="..." />
-        <img src={pradi} className="card-img-top img" alt="..." />
-        <img src={thanu} className="card-img-top img" alt="..." />
-        <img src={powsi} className="card-img-top img" alt="..." />
-        <img src={farhath} className="card-img-top img" alt="..." />
-        <img src={jana} className="card-img-top img" alt="..." />
-        <img src={joshi} className="card-img-top img" alt="..." />
-      </div> */}
     </AdminLayout>
   );
 }
