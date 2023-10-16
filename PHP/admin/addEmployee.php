593021f9d9897033e387af361807b475aa80ae3d
Jana-JR <janarthanan2skr@gmail.com>
2023-10-16 09:33:06 +0530

    addEmployee & employeeList + deleted doctor

--- PHP/admin/addEmployee.php
+++ PHP/admin/addEmployee.php
@@ -1,41 +1,116 @@
 <?php
-
 header("Access-Control-Allow-Origin: http://localhost:3000");
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Healerz";
 
+require_once "../classes/Employee.php";
+use classes\Employee;
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    try {
+        $employee_id = $_POST["employee_ID"];
+        $employee_name = $_POST["employee_name"];
+        $userType = $_POST["userType"];
+        $email = $_POST["email"];
+        $phoneNo = $_POST["phoneNo"];
+        $address = $_POST["address"];
+        $pass = $_POST["password"];
+        $regNo = $_POST["regNo"];
 
+     
+        $employee = new Employee($employee_id, $employee_name, $phoneNo, $email, $address, $pass, $regNo, $userType);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+        // Handle different employee types based on designation
+        switch ($userType) {
+            case 'doctor':
+                
+                $employee_id = $_POST["employee_ID"];
+                $employee_name = $_POST["employee_name"];
+                $userType = $_POST["userType"];
+                $email = $_POST["email"];
+                $phoneNo = $_POST["phoneNo"];
+                $address = $_POST["address"];
+                $pass = $_POST["password"];
+                $regNo = $_POST["regNo"];
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit();
-} else {
-    $pharmacist_ID = $_POST["pharmacist_ID"];
-    $pharmacist_name = $_POST["pharmacist_name"];
-    $designation = $_POST["designation"];
-    $email = $_POST["email"];
-    $phoneNo = $_POST["phoneNo"];
-    $address = $_POST["address"];
-    $password = $_POST["password"];
-    $regNo = $_POST["regNo"];
-    $imageUpload = $_POST["imageUpload"];
-    
-    $sql = "INSERT INTO pharmacist(Pharmacist_ID,Pharmacist_Name,Designation,Email,PhoneNo,Address,Password,SLMC,Image) VALUES ('$pharmacist_ID', '$pharmacist_name','$designation','$email','$phoneNo', '$address', '$password', '$regNo', '$imageUpload');";
-    $res = mysqli_query($conn, $sql);
+                $employee->setEmployee_ID($employee_id);
+                $employee->setEmployee_name($employee_name);
+                $employee->setUserType($userType);
+                $employee->setEmail($email);
+                $employee->setPhoneNo($phoneNo);
+                $employee->setAddress($address);
+                $employee->setPassword($pass);
+                $employee->setRegNo($regNo);
+                break;
 
-    if ($res) {
-        echo json_encode(array("message" => "Pharmacist Added Successfully"));
-    } else {
-        // Check if the error is due to a duplicate primary key
-        if (mysqli_errno($conn) == 1062) {
-            echo json_encode(array("message" => "Pharmacist with the same Pharmacist_ID already exists. Please use a different Pharmacist_ID."));
-        } else {
-            echo json_encode(array("message" => "Error: " . mysqli_error($conn)));
+            case 'pharmacist':
+                
+                $employee_id = $_POST["employee_ID"];
+                $employee_name = $_POST["employee_name"];
+                $userType = $_POST["userType"];
+                $email = $_POST["email"];
+                $phoneNo = $_POST["phoneNo"];
+                $address = $_POST["address"];
+                $pass = $_POST["password"];
+                $regNo = $_POST["regNo"];
+
+                $employee->setEmployee_ID($employee_id);
+                $employee->setEmployee_name($employee_name);
+                $employee->setUserType($userType);
+                $employee->setEmail($email);
+                $employee->setPhoneNo($phoneNo);
+                $employee->setAddress($address);
+                $employee->setPassword($pass);
+                $employee->setRegNo($regNo);
+                break;
+
+            case 'admin':
+                
+                $employee_id = $_POST["employee_ID"];
+                $employee_name = $_POST["employee_name"];
+                $userType = $_POST["userType"];
+                $pass = $_POST["password"];
+
+                $employee->setEmployee_ID($employee_id);
+                $employee->setEmployee_name($employee_name);
+                $employee->setUserType($userType);
+                $employee->setPassword($pass);
+                break;
+                
+            case 'clubadmin':
+                
+                $employee_id = $_POST["employee_ID"];
+                $employee_name = $_POST["employee_name"];
+                $userType = $_POST["userType"];
+                $pass = $_POST["password"];
+
+                $employee->setEmployee_ID($employee_id);
+                $employee->setEmployee_name($employee_name);
+                $employee->setUserType($userType);
+                $employee->setPassword($pass);
+                
+                break;
+            
+        
+            default:
+                // Default case if user type doesn't match any specific type
+                break;
         }
+
+        // Add the employee to the database
+        $res = $employee->addEmployee();
+
+        if ($res) {
+            $response = array("message" => "Employee Added Successfully");
+        } else {
+            $response = array("message" => "Failed to add Employee");
+        }
+
+        echo json_encode($response);
+    } catch (Exception $e) {
+        $response = array("message" => "Error: " . $e->getMessage());
+        echo json_encode($response);
     }
+} else {
+    $response = array("message" => "Invalid request method.");
+    echo json_encode($response);
 }
+?>
--- src/components/admin/EmployeeList.js
+++ src/components/admin/EmployeeList.js
@@ -1,66 +1,242 @@
-import React from 'react'
-import AdminLayout from '../../layouts/AdminLayout'
-import { DataGrid } from '@mui/x-data-grid';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ViewModal from "./ViewEmployeeModal";
+import AdminLayout from "../../layouts/AdminLayout";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import CustomConfirmModal from "./ConfirmDeleteModal";
+import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from '@mui/icons-material/Clear';
+
+function EmployeeList(props) {
+  const [showModal, setShowModal] = useState(false);
+  const [searchTerm3, setSearchTerm] = useState("");
+  const [searchTerm4, setSearchTerm2] = useState("");
+  const [EmployeeList, setEmployeeList] = useState([]);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [confirmModalVisible, setConfirmModalVisible] = useState(false);
+  const [setEmployeeToDelete, setSelectedEmployeeToDelete] = useState(null);
+  const [filteredEmployeeList, setFilteredEmployeeList] = useState([]);
+
+  const handleChange3 = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleChange4 = (event) => {
+    setSearchTerm2(event.target.value);
+  };
 
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'name', headerName: 'Name', width: 130 },
-  { field: 'designation', headerName: 'Designation', width: 130 },
-  { field: 'email', headerName: 'Email', width: 130 },
-  { field: 'phoneno', headerName: 'Last name', width: 130 },
-  { field: 'address', headerName: 'Address', width: 130 },
-  { field: 'regNo', headerName: 'Registration No', width: 130 },
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    const searchedEmployee = EmployeeList.find(
+      (Employee) => Employee.employee_ID === searchTerm3
+    );
+    if (searchedEmployee) {
+      setSelectedEmployee(searchedEmployee);
+      setShowModal(true);
+      setSearchTerm("");
+    } else {
+      toast.error("Invalid Employee ID");
+    }
+  };
 
+  const handleSearchSubmit2 = (event) => {
+    event.preventDefault();
+    const searchedEmployee = EmployeeList.find(
+      (Employee) => Employee.employeeName === searchTerm4
+    );
+    if (searchedEmployee) {
+      setSelectedEmployee(searchedEmployee);
+      setShowModal(true);
+      setSearchTerm2("");
+    } else {
+      toast.error("Invalid Employee Name");
+    }
+  };
 
-  {/*
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-*/}
-];
+  const openModal = (Employee) => {
+    setSelectedEmployee(Employee);
+    setShowModal(true);
+  };
 
-const rows = [
-  { id: 1, name: 'Snow', designation: 'pharmacist', email: 'phar@gmail.com', phoneno: 771235876, address: '05, main road, Badulla', regNo: '4244' },
-  { id: 1, name: 'Snow', designation: 'pharmacist', email: 'phar@gmail.com', phoneno: 771235876, address: '05, main road, Badulla', regNo: '4244' },
-  { id: 1, name: 'Snow', designation: 'pharmacist', email: 'phar@gmail.com', phoneno: 771235876, address: '05, main road, Badulla', regNo: '4244' },
-  { id: 1, name: 'Snow', designation: 'pharmacist', email: 'phar@gmail.com', phoneno: 771235876, address: '05, main road, Badulla', regNo: '4244' },
-  { id: 1, name: 'Snow', designation: 'pharmacist', email: 'phar@gmail.com', phoneno: 771235876, address: '05, main road, Badulla', regNo: '4244' },
-  { id: 1, name: 'Snow', designation: 'pharmacist', email: 'phar@gmail.com', phoneno: 771235876, address: '05, main road, Badulla', regNo: '4244' },
-  { id: 1, name: 'Snow', designation: 'pharmacist', email: 'phar@gmail.com', phoneno: 771235876, address: '05, main road, Badulla', regNo: '4244' },
-  
-];
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+ 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost/Healerz/PHP/admin/displayEmployeelist.php"
+      );
+      setEmployeeList(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
+  const handleDelete = (Employee) => {
+    setConfirmModalVisible(true);
+    setSelectedEmployeeToDelete(Employee);
+  };
 
+  const handleConfirmDelete = async () => {
+    setConfirmModalVisible(false);
+    const EmployeeToDelete = setEmployeeToDelete;
+    setSelectedEmployeeToDelete(null);
+    try {
+      await axios.delete(
+        `http://localhost/Healerz/PHP/admin/deleteemployee.php?Employee_ID=${EmployeeToDelete.employee_ID}`
+      );
+      toast.success("Employee deleted successfully");
+      fetchData();
+    } catch (error) {
+      console.error("Error deleting Employee:", error);
+      toast.error("Error deleting Employee");
+    }
+  };
 
-
-export default function EmployeeList() {
   return (
     <AdminLayout>
-      <div className={"addboxx  mt-4"} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}  >
-        <div className={"Addcontt "}>
-          <div style={{ height: 440, width: '100%' }}>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: { page: 0, pageSize: 5 },
-                },
-              }}
-              pageSizeOptions={[5, 10]}
-              checkboxSelection
-            />
+      {/* <h3 className="serhett">Employee List</h3> */}
+      <div className={"container Employeelisttable"}>
+        <div className={"p-5"}>
+          <hr />
+          <div
+            className={"SearchSection"}
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            {/* <div>
+              <h3 className={"content-heading"}>Filter the Results : </h3>
+            </div> */}
+            <div className="SearchSection2">
+              <div className="search-input-container">
+                <form
+                  onSubmit={handleSearchSubmit}
+                  style={{ display: "flex", flexDirection: "row" }}
+                >
+                  <input
+                    className={"SearchBox1"}
+                    type="text"
+                    placeholder="Filter By Employee ID"
+                    value={searchTerm3}
+                    onChange={handleChange3}
+                    style={{ width: "300px" }}
+                  />
+                  <div className="search-icon" onClick={handleSearchSubmit}>
+                    <SearchIcon />
+                  </div>
+                  {searchTerm3 && (
+                  <div className="search-icon" style={{zIndex:'100',backgroundColor:'white',right:'6px'}} onClick={() => setSearchTerm("")}>
+                   <ClearIcon/>
+                  </div>
+                )}
+                </form>
+              </div>
+              <div className="search-input-container">
+              <form
+                onSubmit={handleSearchSubmit2}
+                style={{ display: "flex", flexDirection: "row" }}
+              >
+                <input
+                  className={"SearchBox1"}
+                  type="text"
+                  placeholder="Filter by Employee Name"
+                  value={searchTerm4}
+                  onChange={handleChange4}
+                  style={{ width: "300px" }}
+                />
+                 <div className="search-icon" onClick={handleSearchSubmit2}>
+                    <SearchIcon />
+                  </div>
+                  {searchTerm4 && (
+                  <div className="search-icon" style={{zIndex:'100',backgroundColor:'white',right:'6px'}} onClick={() => setSearchTerm2("")}>
+                   <ClearIcon/>
+                  </div>
+                )}
+              </form>
+              </div>
+              
+            </div>
+          </div>
+          <hr />
+          <div className="table-containertt">
+            <table className={"table table-hover table-striped"}>
+              <thead
+                className={"top-0 position-sticky h-45"}
+                style={{ zIndex: 100 }}
+              >
+                <tr>
+                  <th scope="col">NO</th>
+                  <th scope="col">Employee_ID</th>
+                  <th scope="col">EmployeeName</th>                
+                  <th scope="col">PhoneNo</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Addess</th>
+                  <th scope="col">SLMC RegNo</th>
+                  <th scope="col">UserType</th>
+                  <th scope="col">ACTION</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredEmployeeList.length > 0 ? (
+                  filteredEmployeeList.map((data, index) => (
+                    <tr key={index}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{data.employee_ID}</td>
+                      <td>{data.employeeName}</td>
+                      <td>{data.phoneNo}</td>
+                      <td>{data.email}</td>
+                      <td>{data.address}</td>
+                      <td>{data.regNo}</td>
+                      <td>{data.userType}</td>
+                      <td>
+                        <IconButton
+                          aria-label="delete"
+                          className="viewbutt"
+                          onClick={() => openModal(data)}
+                          style={{ color: "green" }}
+                        >
+                          <VisibilityIcon />
+                        </IconButton>
+                        <IconButton
+                          aria-label="delete"
+                          className="viewbutt"
+                          onClick={() => handleDelete(data)}
+                          style={{ color: "red" }}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="8">No results found</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
           </div>
         </div>
+        <ToastContainer />
+        <CustomConfirmModal
+          show={confirmModalVisible}
+          onHide={() => setConfirmModalVisible(false)}
+          onConfirm={handleConfirmDelete}
+        />
       </div>
+      <ViewModal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        EmployeeDetails={selectedEmployee}
+      />
     </AdminLayout>
-  )
+  );
 }
+
+export default EmployeeList;
