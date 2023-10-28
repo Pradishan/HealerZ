921b2bc001385f5cd283eadadd3c85168060c611
Powsihan <powsipowsihan07@gmail.com>
2023-10-28 13:27:39 +0530

    some css changes

--- src/components/admin/DashboardAdmin.js
+++ src/components/admin/DashboardAdmin.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import AdminLayout from "../../layouts/AdminLayout";
 import "./Admin.css";
 import BarchartDrugsoutcome from "../inventoryinterface/additional/BarchartDrugsoutcome";
 import EmployeeChart from "./EmployeeChart";
 import Patientcountchart from "./Patientcountchart";
+import axios from "axios";
 
 export default function DashboardAdmin() {
+
+  const [patientCount, setPatientCount] = useState(0);
+
+  useEffect(() => {
+    axios.get("http://localhost/Healerz/PHP/admin/patienttotal.php")
+      .then((response) => {
+        setPatientCount(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching patient count:", error);
+      });
+  }, []);
   return (
     <AdminLayout>
       <div className="dashboradmin" style={{ marginTop: "20px" }}>
@@ -22,6 +35,8 @@ export default function DashboardAdmin() {
         <div className="dashboradmin2" style={{ marginTop: "20px" }}>
           <div className="catbarchartt2">
             <Patientcountchart />
+            <h3>Patient Total</h3>
+            <h2>{patientCount}</h2>
           </div>
         </div>
       </div>
