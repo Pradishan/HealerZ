0638c2021a42abc01b08a13edbc712cf44439446
Jana-JR <janarthanan2skr@gmail.com>
2023-10-29 21:55:21 +0530

    Admin settings

---
+++ PHP/admin/changePassword.php
@@ -0,0 +1,60 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+require '../classes/DBconnector.php';
+use classes\DBconnector;
+
+$con = new DBconnector();
+$conn = $con->getConnection();
+
+
+try {    
+    if ($_SERVER["REQUEST_METHOD"] !== "PUT") {
+        throw new Exception("Invalid request method. Only PUT requests are allowed.");
+    }
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if (!isset($data['employee_ID'])) {
+        throw new Exception('employee_ID or currentPassword is not provided in the request.');
+    }
+
+    //  // Fetch the current password from the database
+    //  $stmt = $conn->prepare("SELECT role FROM employee WHERE employee_ID = :employee_ID");
+    // //  $stmt->bindValue(':employee_ID', $data['employee_ID']);
+    // //  $stmt->execute();
+    // //  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+    //  // Check if the provided current password matches the password in the database
+    // //  if ($data['currentPassword'] !== $result['password']) {
+    // //      throw new Exception('Current password does not match.');
+    // //  }
+
+    //  // Check if the user has the 'admin' role
+    // $requiredRole = 'admin'; // Change this to the required role
+    // if ($result['role'] !== $requiredRole) {
+    //     throw new Exception('Unauthorized. Only admin can change passwords.');
+    // }
+
+    $stmt = $conn->prepare("UPDATE employee SET password = :password WHERE employee_ID = :employee_ID");
+    $stmt->bindValue(':employee_ID', $data['employee_ID']);
+    $stmt->bindValue(':password', $data['newPassword']);
+
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    if ($rowCount > 0) {
+        echo json_encode(array('message' => 'Password changed successfully'));
+    } else {
+        echo json_encode(array('error' => 'Employee not found'));
+    }
+} catch (PDOException $e) {
+    echo json_encode(array('error' => 'Database error: ' . $e->getMessage()));
+} catch (Exception $e) {
+    echo json_encode(array('error' => $e->getMessage()));
+}
+
+?>
