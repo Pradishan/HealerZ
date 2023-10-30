aa7dc6f77617bf12676a351ff6c61d6fd508021a
Powsihan <powsipowsihan07@gmail.com>
2023-10-30 14:30:54 +0530

    inventory dashboard changes

---
+++ PHP/Inventory/dashboard/categorytotal.php
@@ -0,0 +1,25 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+require_once('../../classes/DBconnector.php');
+
+$dbConnector = new classes\DBconnector(); 
+
+$pdo = $dbConnector->getConnection(); 
+
+if (!$pdo) {
+    die("Connection failed.");
+}
+$query = "SELECT COUNT(*) as count FROM drug";
+
+$result = $pdo->query($query);
+
+if ($result) {
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    $patientCount = $row['count'];
+    echo $patientCount;
+} else {
+    echo "Error fetching drug count";
+}
+
+$pdo = null;
+?>
