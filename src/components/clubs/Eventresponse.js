2af72dddb51a0985fcd55271ac19a853d920408d
Powsihan <powsipowsihan07@gmail.com>
2023-10-31 21:25:21 +0530

    css change

--- src/components/clubs/Eventresponse.js
+++ src/components/clubs/Eventresponse.js
@@ -14,6 +14,7 @@ import UpdateModal from "./UpdateEvent";
 import ViewModal from "./EventresponseViewModal";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import RegistrationModal from "./RegistrationModal";
+import "./Clubs.css";
 
 function EventList(props) {
   const [showModal, setShowModal] = useState(false);
