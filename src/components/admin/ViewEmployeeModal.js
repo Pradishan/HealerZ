db5f5d6ed98cdab053648ff9f466de16eff1c6b1
Powsihan <powsipowsihan07@gmail.com>
2023-10-31 14:37:18 +0530

    Clubadmin changes

--- src/components/admin/ViewEmployeeModal.js
+++ src/components/admin/ViewEmployeeModal.js
@@ -1,67 +1,84 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
-
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import { IconButton } from "@mui/material";
+import Person3Icon from "@mui/icons-material/Person3";
 
 function ViewEmployeeModal(props) {
-    const { show, onHide, EmployeeDetails } = props;
+  const { show, onHide, EmployeeDetails } = props;
 
-    return (
-        <Modal show={show} onHide={onHide} centered>
-            <Modal.Header closeButton>
-                <Modal.Title>Employee Detail</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <hr />
-                <table>
-                    <tbody>
-                        <tr>
-                            <th className={'detailhed'}>Employee_ID</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{EmployeeDetails && EmployeeDetails.employee_ID}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Employee_Name</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{EmployeeDetails && EmployeeDetails.employee_Name}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Designation</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{EmployeeDetails && EmployeeDetails.role}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Email</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{EmployeeDetails && EmployeeDetails.Email}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>PhoneNo</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{EmployeeDetails && EmployeeDetails.PhoneNo}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Address</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{EmployeeDetails && EmployeeDetails.Address}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>SLMC RegNo</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{EmployeeDetails && EmployeeDetails.SLMC}</td>
-                        </tr>
-                        {/* Add more attributes here */}
-                    </tbody>
-                </table>
-                
-                <hr />
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={onHide}>
-                    Close
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+  return (
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Header>
+        <Modal.Title className="modaltitleee">Employee Detail</Modal.Title>
+        <IconButton>
+          <Person3Icon
+            style={{ color: "darkgreen" }}
+            sx={{ fontSize: "40px" }}
+          />
+        </IconButton>
+      </Modal.Header>
+      <Modal.Body>
+        <table>
+          <tbody>
+            <tr>
+              <th className={"detailhed"}>Employee_ID</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {EmployeeDetails && EmployeeDetails.employee_ID}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Employee_Name</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {EmployeeDetails && EmployeeDetails.employee_Name}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Designation</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {EmployeeDetails && EmployeeDetails.role}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Email</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {EmployeeDetails && EmployeeDetails.Email}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>PhoneNo</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {EmployeeDetails && EmployeeDetails.PhoneNo}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Address</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {EmployeeDetails && EmployeeDetails.Address}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>SLMC RegNo</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {EmployeeDetails && EmployeeDetails.SLMC}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default ViewEmployeeModal;
--- src/components/admin/ViewPatientModal.js
+++ src/components/admin/ViewPatientModal.js
@@ -1,71 +1,91 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
-
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import { IconButton } from "@mui/material";
+import Person3Icon from "@mui/icons-material/Person3";
 
 function ViewPatientModal(props) {
-    const { show, onHide, PatientDetails } = props;
+  const { show, onHide, PatientDetails } = props;
 
-    return (
-        <Modal show={show} onHide={onHide} centered>
-            <Modal.Header closeButton>
-                <Modal.Title className="modaltitleee">Patient Detail</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <hr />
-                <table>
-                    <tbody>
-                        <tr>
-                            <th className={'detailhed'}>Patient_ID</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{PatientDetails && PatientDetails.Patient_ID}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Patient_Name</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{PatientDetails && PatientDetails.PatientName}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>DateOfBirth</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{PatientDetails && PatientDetails.DateOfBirth}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Gender</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{PatientDetails && PatientDetails.Gender}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>PhoneNo</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{PatientDetails && PatientDetails.PhoneNo}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Email</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{PatientDetails && PatientDetails.Email}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>Address</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet3'}>{PatientDetails && PatientDetails.Address}</td>
-                        </tr>
-                        <tr>
-                            <th className={'detailhed'}>BloodGroup</th>
-                            <th className={'detailspac'}>:</th>
-                            <td className={'detaildet'}>{PatientDetails && PatientDetails.BloodGroup}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                
-                <hr />
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="primary" onClick={onHide}>
-                    Close
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+  return (
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Header>
+        <Modal.Title className="modaltitleee">Patient Detail</Modal.Title>
+        <IconButton>
+          <Person3Icon
+            style={{ color: "darkgreen" }}
+            sx={{ fontSize: "40px" }}
+          />
+        </IconButton>
+      </Modal.Header>
+      <Modal.Body>
+        <table>
+          <tbody>
+            <tr>
+              <th className={"detailhed"}>Entroll_No</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {PatientDetails && PatientDetails.Patient_ID}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Patient_Name</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {PatientDetails && PatientDetails.PatientName}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>DateOfBirth</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {PatientDetails && PatientDetails.DateOfBirth}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Gender</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {PatientDetails && PatientDetails.Gender}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>PhoneNo</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {PatientDetails && PatientDetails.PhoneNo}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Email</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {PatientDetails && PatientDetails.Email}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>Address</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet3"}>
+                {PatientDetails && PatientDetails.Address}
+              </td>
+            </tr>
+            <tr>
+              <th className={"detailhed"}>BloodGroup</th>
+              <th className={"detailspac"}>:</th>
+              <td className={"detaildet"}>
+                {PatientDetails && PatientDetails.BloodGroup}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default ViewPatientModal;
